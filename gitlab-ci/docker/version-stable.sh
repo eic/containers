ce01b8f8f343a59fb4fbea6c867df83fb35c6b3c
Sylvester Joosten <sylvester.joosten@gmail.com>
2021-05-18 16:17:37 +0000

    Resolve "Simplify and consolidate container build"
    New container names are jug_dev and jug_xl (dressed container)

--- gitlab-ci/docker/version-stable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## print the stable version tag for this version to the console
-
-VERSION=`head -n1 ../../VERSION`
-echo "${VERSION%.*}-stable"
--- gitlab-ci/docker/version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-## print the full version number to the console
-
-head -n1 ../../VERSION
