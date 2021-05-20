68c199492d251caeb02a014ee0ea10684a980626
Sylvester Joosten <sjoosten@anl.gov>
2021-05-20 03:57:28 +0000

    Add shell installer, make install.py standalone

--- install/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-## eic_container: Argonne Universal EIC Container
-
-from install.util import smart_mkdir, project_version
-from install.launcher import make_launcher
-from install.modulefile import make_modulefile
