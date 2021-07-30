ca011f279fc6339918aed9e6985086dc2cd49947
Sylvester Joosten <sylvester.joosten@gmail.com>
2021-07-30 18:48:50 +0000

    Abort bash scripts on errors to make CI fail more reliably

--- containers/jug/profile.d/z11_jug_env.sh
+++ containers/jug/profile.d/z11_jug_env.sh
@@ -7,3 +7,8 @@ if [ ! -z ${ATHENA_PREFIX} ]; then
     export PATH=$ATHENA_PREFIX/bin:$PATH
   fi
 fi
+
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+set -E
