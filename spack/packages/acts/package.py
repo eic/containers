267c3efb6560c454c9209f02c41eb3804cbcd29a
Wouter Deconinck <wouter.deconinck@umanitoba.ca>
2021-09-26 13:29:22 +0000

    Update acts/package.py with current up to v12.0.1

--- spack/packages/acts/package.py
+++ spack/packages/acts/package.py
@@ -29,14 +29,24 @@ class Acts(CMakePackage, CudaPackage):
     propagation and fitting, basic seed finding algorithms.
     """
 
-    homepage = "http://acts.web.cern.ch/ACTS/"
+    homepage = "https://acts.web.cern.ch/ACTS/"
     git      = "https://github.com/acts-project/acts.git"
     maintainers = ['HadrienG2']
 
     tags = ['hep']
 
     # Supported Acts versions
-    version('master', branch='master')
+    version('main', branch='main')
+    version('master', branch='main', deprecated=True)  # For compatibility
+    version('13.0.0', commit='ad05672e48b693fd37156f1ad62ed57aa82f858c', submodules=True)
+    version('12.0.1', commit='a80d1ef995d8cdd4190cc09cb249276a3e0161f4', submodules=True)
+    version('12.0.0', commit='e0aa4e7dcb70df025576e050b6e652a2f736454a', submodules=True)
+    version('11.0.0', commit='eac3def261f65b343af6d8ce4bc40443ac57b57e')
+    version('10.0.0', commit='9bfe0b83f277f686408b896a84d2b9b53610f623')
+    version('9.02.0', commit='c438ee490e94eaf1c854a336ef54f398da637a48')
+    version('9.01.0', commit='bf8fd4c03dd94f497d8501df510d8f6a48434afd')
+    version('9.00.1', commit='7d59bc508d898d2cb67ba05a7150a978b9fcc32d')
+    version('9.00.0', commit='e6e3092bf3a9411aac7c11a24d7586abddb75d59')
     version('8.03.0p1', commit='601c0a18b6738cae81c3e23422cfeb3ec7bddce9')
     version('8.03.0', commit='601c0a18b6738cae81c3e23422cfeb3ec7bddce9')
     version('8.02.0p1', commit='f25cf639915fc2ac65b03882ad3eb11fb037ed00')
