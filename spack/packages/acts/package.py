1be738e484eba3bc281cbedf47b3d49df31435d5
Wouter Deconinck <wdconinc@gmail.com>
2022-01-15 23:44:45 +0000

    acts@15.0.0

---
+++ spack/packages/acts/package.py
@@ -0,0 +1,8 @@
+from spack import *
+from spack.pkg.builtin.acts import Acts as BuiltinActs
+
+
+class Acts(BuiltinActs):
+    version('16.0.0', commit='9bd86921155e708189417b5a8019add10fd5b273', submodules=True)
+    version('15.1.0', commit='a96e6db7de6075e85b6d5346bc89845eeb89b324', submodules=True)
+    version('15.0.0', commit='0fef9e0831a90e946745390882aac871b211eaac', submodules=True)
