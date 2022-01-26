a7f9cb63383a88bf190c8aad0d84fe3c7c521aec
Wouter Deconinck <wdconinc@gmail.com>
2022-01-26 01:26:12 +0000

    acts/package.py: remove some import lines

--- spack/packages/acts/package.py
+++ spack/packages/acts/package.py
@@ -1,7 +1,4 @@
-from spack import *
 from spack.pkg.builtin.acts import Acts as BuiltinActs
-
-
 class Acts(BuiltinActs):
     version('16.0.0', commit='9bd86921155e708189417b5a8019add10fd5b273', submodules=True)
     version('15.1.0', commit='a96e6db7de6075e85b6d5346bc89845eeb89b324', submodules=True)
