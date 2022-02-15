8e4b6294ddad496de2b2c2bed5c483c551b4ec31
Wouter Deconinck <wdconinc@gmail.com>
2022-02-15 00:55:10 +0000

    dd4hep: new versions 1.19 and 1.20

--- spack/packages/dd4hep/package.py
+++ spack/packages/dd4hep/package.py
@@ -3,12 +3,6 @@ from spack.pkg.builtin.dd4hep import Dd4hep as BuiltinDd4hep
 
 
 class Dd4hep(BuiltinDd4hep):
-    # custom hash for the 2021-07-27 version, needed to include
-    # https://github.com/AIDASoft/DD4hep/pull/849
-    # https://github.com/AIDASoft/DD4hep/pull/851
-    patch('2021-07-27.patch', when='@1.17')
-    # patch for https://github.com/AIDASoft/DD4hep/issues/862
-    patch('0001-do-not-change-momentum-in-getParticleDirection.patch', when='@1.17:1.18')
-    # hack to fix refcount underflow
-    patch('refcount_underflow.patch', when='@1.17:')
-    patch('pdg.patch', when='@1.17:')
+    version('1.20', sha256='cf6af0c486d5c84e8c8a8e40ea16cec54d4ed78bffcef295a0eeeaedf51cab59')
+    version('1.19', sha256='d2eccf5e8402ba7dab2e1d7236e12ee4db9b1c5e4253c40a140bf35580db1d9b')
+
