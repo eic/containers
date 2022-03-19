0e09507e232f32c743fcb7c01e4aa0b38e931f45
Wouter Deconinck <wdconinc@gmail.com>
2022-03-19 02:55:03 +0000

    dd4hep: patch for MCParticles parent and daughter relations

--- spack/packages/dd4hep/package.py
+++ spack/packages/dd4hep/package.py
@@ -3,6 +3,10 @@ from spack.pkg.builtin.dd4hep import Dd4hep as BuiltinDd4hep
 
 
 class Dd4hep(BuiltinDd4hep):
+    version('1.20.1', sha256='18c18a125583c39cb808c602e052cc2379aa3a8029aa78dbb40bcc31f1deb798')
     version('1.20', sha256='cf6af0c486d5c84e8c8a8e40ea16cec54d4ed78bffcef295a0eeeaedf51cab59')
     version('1.19', sha256='d2eccf5e8402ba7dab2e1d7236e12ee4db9b1c5e4253c40a140bf35580db1d9b')
 
+    patch('https://github.com/AIDASoft/DD4hep/pull/896.diff',
+          sha256='2d7e87824d324b8bd14cb2a8b441d2fc25a6d3474e6e041bd68c56439a9477cf',
+          when='@1.20:1.20.1')
