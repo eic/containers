ba68da4d423434807c3153206aef7ec360ca2640
Wouter Deconinck <wdconinc@gmail.com>
2022-05-12 01:03:35 +0000

    Acts@19.0.0 *not working with juggler yet

--- spack/packages/acts/package.py
+++ spack/packages/acts/package.py
@@ -1,5 +1,6 @@
 from spack.pkg.builtin.acts import Acts as BuiltinActs
 class Acts(BuiltinActs):
+    version('19.0.0', commit='1ce9c583150060ba8388051685433899713d56d9', submodules=True)
     version('18.0.0', commit='fe03b5af6ca2b092dec87c4cef77dd552bbbe719', submodules=True)
     version('17.1.0', commit='0d9c3a6da022da48d6401e10c273896a1f775a9e', submodules=True)
     version('17.0.0', commit='ccbf4c7d4ec3698bac4db9687fab2455a3f9c203', submodules=True)
