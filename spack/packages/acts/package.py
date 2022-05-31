54deeaf9e55707bc8159e202bded07a60d2ee30c
Wouter Deconinck <wdconinc@gmail.com>
2022-05-31 01:01:25 +0000

    Spack v0.18.0

--- spack/packages/acts/package.py
+++ spack/packages/acts/package.py
@@ -1,9 +1,3 @@
 from spack.pkg.builtin.acts import Acts as BuiltinActs
 class Acts(BuiltinActs):
-    version('19.0.0', commit='1ce9c583150060ba8388051685433899713d56d9', submodules=True)
-    version('18.0.0', commit='fe03b5af6ca2b092dec87c4cef77dd552bbbe719', submodules=True)
-    version('17.1.0', commit='0d9c3a6da022da48d6401e10c273896a1f775a9e', submodules=True)
-    version('17.0.0', commit='ccbf4c7d4ec3698bac4db9687fab2455a3f9c203', submodules=True)
-    version('16.0.0', commit='9bd86921155e708189417b5a8019add10fd5b273', submodules=True)
-    version('15.1.0', commit='a96e6db7de6075e85b6d5346bc89845eeb89b324', submodules=True)
-    version('15.0.0', commit='0fef9e0831a90e946745390882aac871b211eaac', submodules=True)
+    version('19.1.0', commit='82f42a2cc80d4259db251275c09b84ee97a7bd22', submodules=True)
--- spack/packages/gaudi/package.py
+++ spack/packages/gaudi/package.py
@@ -1,7 +1,6 @@
 from spack import *
 from spack.pkg.builtin.gaudi import Gaudi as BuiltinGaudi
 class Gaudi(BuiltinGaudi):
-    version('36.4', sha256='1a5c27cdc21ec136b47f5805406c92268163393c821107a24dbb47bd88e4b97d')
-    version('36.3', sha256='9ac228d8609416afe4dea6445c6b3ccebac6fab1e46121fcc3a056e24a5d6640')
-    version('36.2', sha256='a1b4bb597941a7a5b8d60382674f0b4ca5349c540471cd3d4454efbe7b9a09b9')
-    version('36.1', sha256='9f718c832313676249e5c3ac76ba4346978ee2328f8cdcb29176498b080402e9')
+    patch('https://gitlab.cern.ch/gaudi/Gaudi/-/commit/03db151003d5207795ae8ec96aaf2f5f8a9ea008.diff',
+          sha256='e73f45f10ea4a143a05c53a468233ceaad1a8ec22dee9877d178a3c9ee46c70f',
+          when='@:36.5')
