2fb34d701c676d13fee7c0d9bf004859ce1f8bb6
Wouter Deconinck <wdconinc@gmail.com>
2023-07-20 19:19:27 +0000

    fix: ensure freetype installs shared library

--- spack.sh
+++ spack.sh
@@ -11,9 +11,11 @@ read -r -d '' SPACK_CHERRYPICKS <<- \
 9ee2d79de172de14477a78e5d407548a63eea33a
 776ab132760d63eab0703b7c0ebebc72a8443f5b
 188168c476eabe99764634db8d78eb3f9ea2a927
+b3268c2703b84f4e4961c1e2cdf43e8998f283e6
 ---
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
 ## 9ee2d79de172de14477a78e5d407548a63eea33a: libxpm package: fix RHEL8 build with libintl
 ## 776ab132760d63eab0703b7c0ebebc72a8443f5b: [xrootd] New variants, new version, improve build config
 ## 188168c476eabe99764634db8d78eb3f9ea2a927: podio: Add 0.16.5 tag
+## b3268c2703b84f4e4961c1e2cdf43e8998f283e6: freetype: add pic and shared variants
