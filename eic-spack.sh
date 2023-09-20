19a4228b7f91bc8d267739fc2071b0e704f7517c
Wouter Deconinck <wdconinc@gmail.com>
2023-09-20 12:05:04 +0000

    test: use cherry-picks

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,11 +3,13 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack github version, e.g. v0.19.7 or commit hash
 ## note: nightly builds will use e.g. releases/v0.19
-EICSPACK_VERSION="dd4hep-pr1161"
+EICSPACK_VERSION="v0.20.18"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
 --- || true
+e976b3790a2fe6193d58484c473110bc8d089ee4
+3913a77446e4318962e546eb3159e7127967c860
 ---
 ## Ref: https://github.com/eic/eic-spack/commit/[hash]
 ## [hash]: [description]
