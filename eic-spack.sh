e2d40cdba313ffb97565cb52b64dbc8e17df2dd6
Wouter Deconinck <wdconinc@gmail.com>
2023-09-20 12:25:19 +0000

    fix: add cherry pick with correct sha256

--- eic-spack.sh
+++ eic-spack.sh
@@ -10,6 +10,7 @@ read -r -d '' EICSPACK_CHERRYPICKS <<- \
 --- || true
 e976b3790a2fe6193d58484c473110bc8d089ee4
 3913a77446e4318962e546eb3159e7127967c860
+53064865f6fd44035ba0286cd09b0990dc3ddd08
 ---
 ## Ref: https://github.com/eic/eic-spack/commit/[hash]
 ## [hash]: [description]
