f5f57ec3283c4d291318214c6818f86727195f74
Wouter Deconinck <wdconinc@gmail.com>
2023-10-06 01:02:19 +0000

    chore: spack-v0.20.2

--- spack.sh
+++ spack.sh
@@ -3,12 +3,11 @@ SPACK_ORGREPO="spack/spack"
 
 ## Spack github version, e.g. v0.18.1 or commit hash
 ## note: nightly builds will use e.g. releases/v0.19
-SPACK_VERSION="v0.20.1"
+SPACK_VERSION="v0.20.2"
 
 ## Space-separated list of spack cherry-picks
 read -r -d '' SPACK_CHERRYPICKS <<- \
 --- || true
-ef5d110d4abd2411d6bde82d738b205eb6672fe8
 9ee2d79de172de14477a78e5d407548a63eea33a
 776ab132760d63eab0703b7c0ebebc72a8443f5b
 188168c476eabe99764634db8d78eb3f9ea2a927
@@ -26,11 +25,9 @@ ea1439dfa11a3996c9927ed792dc9fe4b7efc1b8
 ed76eab6943221f17776fd8d128ade6ba69e492c
 6c5d125cb06a86ce05bec27ae9fb9b07103bc1c5
 e3e7609af4903be7df42b6ae5ccf9a20293503d2
-df4a2457a41e7ab634e86d3148d8b22a9f433a6a
 ---
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
-## ef5d110d4abd2411d6bde82d738b205eb6672fe8: Fix multiple quadratic complexity issues in environments
 ## 9ee2d79de172de14477a78e5d407548a63eea33a: libxpm package: fix RHEL8 build with libintl
 ## 776ab132760d63eab0703b7c0ebebc72a8443f5b: [xrootd] New variants, new version, improve build config
 ## 188168c476eabe99764634db8d78eb3f9ea2a927: podio: Add 0.16.5 tag
@@ -48,4 +45,3 @@ df4a2457a41e7ab634e86d3148d8b22a9f433a6a
 ## ed76eab6943221f17776fd8d128ade6ba69e492c: geant4: new version 11.1.2
 ## 6c5d125cb06a86ce05bec27ae9fb9b07103bc1c5: cernlib: new variant shared
 ## e3e7609af4903be7df42b6ae5ccf9a20293503d2: edm4hep: Add version 0.9
-## df4a2457a41e7ab634e86d3148d8b22a9f433a6a: Fix broken semver regex
