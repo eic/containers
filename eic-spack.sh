45764f467c1a41469d9375fd6859ff8f328d8b93
Wouter Deconinck <wdconinc@gmail.com>
2023-11-30 05:08:36 +0000

    chore: eic-spack-e77ace878ad5aa3a6e54c4ba4db0ca304043a15d

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="4c9c1250d054e587455e5f00df242e376018130a"
+6EICSPACK_VERSION="4c9c1250d054e587455e5f00df242e376018130a"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
