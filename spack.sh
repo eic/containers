c3f8371ea03ad438ed9064fd5a41b83a61dc9807
Wouter Deconinck <wdconinc@gmail.com>
2023-12-17 18:38:52 +0000

    fix: spack cherry-pick py-htgettoken merge commit

--- spack.sh
+++ spack.sh
@@ -16,11 +16,9 @@ c485709f625429a88a184a099373d76c9438f8e3
 6f08daf67020289e6a5ed1df9783ac5b2919e477
 50051b56199992eb4395b8ff22913c1995311a8c
 f01774f1d41781bc4b9e5abb5469e234168da663
-cfa2f19f48699a762af77ece8a9c5bc4db3a75d5
-2433f503b73bb515b2e1a7615ae01fea59d2290e
 16f4c53cd4cfb4bc6c9390f6e65217fc9ccc58c9
 d171f314c77ba61b3cd780f159afe6abced5707d
-a0129204c6fbacfaaa44e4f081ace91620efd457
+b111064e221aae83e62226672cd8bf9a7524423d
 ---
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
@@ -32,8 +30,6 @@ a0129204c6fbacfaaa44e4f081ace91620efd457
 ## 6f08daf67020289e6a5ed1df9783ac5b2919e477: root: add a webgui patch
 ## 50051b56199992eb4395b8ff22913c1995311a8c: geant4: new version 11.1.3
 ## f01774f1d41781bc4b9e5abb5469e234168da663: hepmc3: fix from_variant -> self.define
-## cfa2f19f48699a762af77ece8a9c5bc4db3a75d5: py-htgettoken: use os.environ, avoid AttributeError
-## 2433f503b73bb515b2e1a7615ae01fea59d2290e: py-htgettoken: allow for undefined variables
 ## 16f4c53cd4cfb4bc6c9390f6e65217fc9ccc58c9: py-bokeh: new version 3.3.1, and supporting packages
 ## d171f314c77ba61b3cd780f159afe6abced5707d: py-pygithub: new versions, dependencies
-## a0129204c6fbacfaaa44e4f081ace91620efd457: py-htgettoken: use dict get()
+## b111064e221aae83e62226672cd8bf9a7524423d: py-htgettoken: use os.environ, avoid AttributeError
