4d2299fbbe114db86d626af15c89949b2bbcbd53
Wouter Deconinck <wdconinc@gmail.com>
2023-12-17 17:37:07 +0000

    fix: py-htgettoken: use dict get()

--- spack.sh
+++ spack.sh
@@ -20,6 +20,7 @@ cfa2f19f48699a762af77ece8a9c5bc4db3a75d5
 2433f503b73bb515b2e1a7615ae01fea59d2290e
 16f4c53cd4cfb4bc6c9390f6e65217fc9ccc58c9
 d171f314c77ba61b3cd780f159afe6abced5707d
+a0129204c6fbacfaaa44e4f081ace91620efd457
 ---
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
@@ -35,3 +36,4 @@ d171f314c77ba61b3cd780f159afe6abced5707d
 ## 2433f503b73bb515b2e1a7615ae01fea59d2290e: py-htgettoken: allow for undefined variables
 ## 16f4c53cd4cfb4bc6c9390f6e65217fc9ccc58c9: py-bokeh: new version 3.3.1, and supporting packages
 ## d171f314c77ba61b3cd780f159afe6abced5707d: py-pygithub: new versions, dependencies
+## a0129204c6fbacfaaa44e4f081ace91620efd457: py-htgettoken: use dict get()
