bc6acaf7d1bda9856222bb766190146c6563b056
Wouter Deconinck <wdconinc@gmail.com>
2023-12-18 17:16:51 +0000

    fix: spack cherry-pick py-torch: move patch before def patch

--- spack.sh
+++ spack.sh
@@ -22,6 +22,7 @@ b111064e221aae83e62226672cd8bf9a7524423d
 3c54177c5d9032cb36cf154b553d739cbeb2d024
 c07ddf83c32b7129247fe90eed486dd844047087
 00875fbc749a7e4e979c35d59c0a8d60d32d4cd7
+64cf67822471ec2d3df2625a6d713f80cbe7ff43
 ---
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
@@ -39,3 +40,4 @@ c07ddf83c32b7129247fe90eed486dd844047087
 ## 3c54177c5d9032cb36cf154b553d739cbeb2d024: edm4hep: add latest tag for 0.10.2
 ## c07ddf83c32b7129247fe90eed486dd844047087: py-torch: set env OpenBLAS_HOME
 ## 00875fbc749a7e4e979c35d59c0a8d60d32d4cd7: py-torch: patch for ${OpenBLAS_HOME}/include/openblas
+## 64cf67822471ec2d3df2625a6d713f80cbe7ff43: py-torch: move patch before def patch
