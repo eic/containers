128c84af50a93edb73fa9c445ca3e86a9a08fad0
Sakib Rahman <rahmans@myumanitoba.ca>
2023-12-18 22:42:26 +0000

    Add py-torch to container

--- spack.sh
+++ spack.sh
@@ -20,6 +20,9 @@ f01774f1d41781bc4b9e5abb5469e234168da663
 d171f314c77ba61b3cd780f159afe6abced5707d
 b111064e221aae83e62226672cd8bf9a7524423d
 3c54177c5d9032cb36cf154b553d739cbeb2d024
+c07ddf83c32b7129247fe90eed486dd844047087
+00875fbc749a7e4e979c35d59c0a8d60d32d4cd7
+64cf67822471ec2d3df2625a6d713f80cbe7ff43
 ---
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
@@ -35,3 +38,6 @@ b111064e221aae83e62226672cd8bf9a7524423d
 ## d171f314c77ba61b3cd780f159afe6abced5707d: py-pygithub: new versions, dependencies
 ## b111064e221aae83e62226672cd8bf9a7524423d: py-htgettoken: use os.environ, avoid AttributeError
 ## 3c54177c5d9032cb36cf154b553d739cbeb2d024: edm4hep: add latest tag for 0.10.2
+## c07ddf83c32b7129247fe90eed486dd844047087: py-torch: set env OpenBLAS_HOME
+## 00875fbc749a7e4e979c35d59c0a8d60d32d4cd7: py-torch: patch for ${OpenBLAS_HOME}/include/openblas
+## 64cf67822471ec2d3df2625a6d713f80cbe7ff43: py-torch: move patch before def patch
