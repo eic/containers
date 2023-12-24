4b0b7a077a3e2aecf11e9b95fea8047ff6ce5c13
Wouter Deconinck <wdconinc@gmail.com>
2023-12-24 19:45:05 +0000

    chore: juggler-v13.0.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f3e18d6a2273029d16462575fe6cc4f4e1efeee0"
+EICSPACK_VERSION="5d8ba97ea6b021a50aa11e924a55a4726c689d65"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
