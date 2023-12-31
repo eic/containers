4809587f481ba8e9a07e13bf6f196d2a7af4e6d4
Wouter Deconinck <wdconinc@gmail.com>
2023-12-31 05:01:42 +0000

    feat: onnx-1.15.0

--- spack.sh
+++ spack.sh
@@ -26,6 +26,7 @@ c07ddf83c32b7129247fe90eed486dd844047087
 7a0c4e8017033430e5f15ed628be6b539e935ba9
 48fcfda1e7c1781cab4cada6d099823b263ab0cc
 8c29e90fa9962f4a44f39f47217b46c85176af28
+0fed2d66bf0eec799707dd1b88ac9419f6ae14e1
 ---
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
@@ -47,3 +48,4 @@ c07ddf83c32b7129247fe90eed486dd844047087
 ## 7a0c4e8017033430e5f15ed628be6b539e935ba9: acts: new versions 31.*
 ## 48fcfda1e7c1781cab4cada6d099823b263ab0cc: setup-env.sh: if exe contains qemu, use /proc/$$/comm instead
 ## 8c29e90fa9962f4a44f39f47217b46c85176af28: Build cache: make signed/unsigned a mirror property
+## 0fed2d66bf0eec799707dd1b88ac9419f6ae14e1: (py-)onnx: new version 1.14.{0,1}, 1.15.0
