a16906f71777c4f580f2742f88faae2bbc8c732e
Wouter Deconinck <wdconinc@gmail.com>
2024-03-10 18:50:36 +0000

    chore: eic-spack: new version of epic-eic

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="0a7c8b2eaaac6de38ca13a1dd8b7f192edf70a7e"
+EICSPACK_VERSION="5a7e0e41ab9a8a6cbeda6b2f2c68129ce178f578"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
