952327d3d1fc9be0ae9c7d72acee47aa25425f34
Wouter Deconinck <wdconinc@gmail.com>
2024-03-26 19:19:22 +0000

    chore: eic-spack root: add 6.30 forward compatibility fix

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="5a7e0e41ab9a8a6cbeda6b2f2c68129ce178f578"
+EICSPACK_VERSION="23e83fea6641a0771d85cecaae1f9a4f3c084bfb"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
