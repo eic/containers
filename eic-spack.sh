9a3ce539ed38b2ba8f434b05bb255cc7a58dd8c4
Wouter Deconinck <wdconinc@gmail.com>
2024-04-07 02:52:02 +0000

    chore: eic-spack eicrecon: depends_on py-onnxruntime when @1.12:

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="23e83fea6641a0771d85cecaae1f9a4f3c084bfb"
+EICSPACK_VERSION="85287c4753174c59f6708e49b83f2c6f4f1014eb"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
