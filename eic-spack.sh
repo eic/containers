332e43a4fb6ccb7d0e36d1a2916cb762329e51a9
Dmitry Kalinkin <dkalinkin@bnl.gov>
2024-04-09 22:42:31 +0000

    bump npsim to 1.4.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="cf2549c097e51e3f1ed42fc8988961736406d332"
+EICSPACK_VERSION="51841e81dc80250c0cad5f69349aa8ed7fab7cd0"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
