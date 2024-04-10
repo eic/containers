40992ccbe53b7c916ae891024eb8db2d728fdea2
Dmitry Kalinkin <dkalinkin@bnl.gov>
2024-04-10 22:59:45 +0000

    chore: eicrecon-1.12.0, epic-eic-24.04.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="85287c4753174c59f6708e49b83f2c6f4f1014eb"
+EICSPACK_VERSION="51841e81dc80250c0cad5f69349aa8ed7fab7cd0"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
