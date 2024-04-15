36a351f6f0f2c57d123df9fd923db348abf0418f
Wouter Deconinck <wdconinc@gmail.com>
2024-04-15 17:04:28 +0000

    chore: eic-spack: npsim: new version 1.4.1

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="51841e81dc80250c0cad5f69349aa8ed7fab7cd0"
+EICSPACK_VERSION="3913c7fc5f45f719d6f74c86b4f08191ad6a5513"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
