0cd98b73caf7f6fdc3c64d525ae9ba28b280f400
Wouter Deconinck <wdconinc@gmail.com>
2024-04-21 20:04:00 +0000

    chore: eic-spack: epic-eic: ensure ROOT libs are found in LD_LIBRARY_PATH before 24.04

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="33b156ce3c01b6c8c5b72107e26cb83b81b0f34c"
+EICSPACK_VERSION="c83030f3a62445c8422f2bbdfd76773752711815"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
