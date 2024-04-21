f3bf1496bdbeaf6d6b36903a41bb59e298d24fc8
Wouter Deconinck <wdconinc@gmail.com>
2024-04-21 23:43:36 +0000

    chore: eic-spack: epic-eic: ensure fmt libs are found in LD_LIBRARY_PATH before 24.04

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="c83030f3a62445c8422f2bbdfd76773752711815"
+EICSPACK_VERSION="b1cabf727298b71b53aa1c2e418139e1489c8241"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
