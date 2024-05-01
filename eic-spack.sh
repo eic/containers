bdcc487731c5848cd72bffb3de3641c292d2e0e6
Wouter Deconinck <wdconinc@gmail.com>
2024-05-01 14:19:05 +0000

    fix: eic-spack: cherry-pick: jana2: retagged 2.2.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f0dc7dae4217d370942145ae47aa173e3db81964"
+EICSPACK_VERSION="f8a71edfd1c385b144f574a1b1eb6ba4e6c5b518"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
