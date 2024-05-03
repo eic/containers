0beed534673be5aa8cb2062dae9516be56bee4cf
Wouter Deconinck <wdconinc@gmail.com>
2024-05-02 22:22:01 -0500

    fix: eic-spack: jana2: retagged 2.2.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f8a71edfd1c385b144f574a1b1eb6ba4e6c5b518"
+EICSPACK_VERSION="c4cd56ba92ae1501fcfd4d67d52d1309f46b98fe"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
