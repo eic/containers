fbba2e1dd31260708ef64b44a753e1639df327d7
Dmitry Kalinkin <dkalinkin@bnl.gov>
2024-05-12 20:59:03 +0000

    Update 2 files
    - /eic-spack.sh
    - /spack-environment/packages.yaml

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="dd3c695f3a373a16d5906a8efc28e8bc21f0cebe"
+EICSPACK_VERSION="2ebf5fc7bb046a8db726f035f25bde036f99ab4f"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
