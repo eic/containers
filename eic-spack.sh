4ab3b35108f1d1a0562701376fa666bb777b855f
Wouter Deconinck <wdconinc@gmail.com>
2024-05-13 12:11:20 +0000

    fix: eic-spack 873b01281d2114b96f87b0b47e64c5ec1f867a95

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="b18a371e7b6dd69791b48db491a91ca166405ef4"
+EICSPACK_VERSION="873b01281d2114b96f87b0b47e64c5ec1f867a95"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
