a6d2aa751fcdff2f1461065e6f8fd4fe7fe13793
Wouter Deconinck <wdconinc@gmail.com>
2024-05-18 19:31:53 +0000

    fix: juggler-14.0.1

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,7 +3,7 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="157326983623a834661bc4bdbb9061bd256f8d1b"
+EICSPACK_VERSION="c839bf3c62184ca7b916efd807c8fb8a80b3042a"
 
 ## Space-separated list of eic-spack cherry-picks
 read -r -d '' EICSPACK_CHERRYPICKS <<- \
