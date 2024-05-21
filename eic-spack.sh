7a52a646cbe1a5443e385933aa26cf33f2b1600a
Wouter Deconinck <wdconinc@gmail.com>
2024-05-21 01:07:02 +0000

    (hot)fix: pin key4hep-spack to yesterday

--- eic-spack.sh
+++ eic-spack.sh
@@ -4,10 +4,3 @@ EICSPACK_ORGREPO="eic/eic-spack"
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
 EICSPACK_VERSION="157326983623a834661bc4bdbb9061bd256f8d1b"
-
-## Space-separated list of eic-spack cherry-picks
-read -r -d '' EICSPACK_CHERRYPICKS <<- \
---- || true
----
-## Ref: https://github.com/eic/eic-spack/commit/[hash]
-## [hash]: [description]
---
+++ key4hep-spack.sh
@@ -0,0 +1,6 @@
+## Key4HEP spack organization and repository, e.g. key4hep/key4hep-spack
+KEY4HEPSPACK_ORGREPO="key4hep/key4hep-spack"
+
+## Key4HEP spack commit hash or github version, e.g. v0.19.7
+## note: nightly builds could use a branch e.g. releases/v0.19
+KEY4HEPSPACK_VERSION="ce50d74dba7c665157cbef63cd5c9cc4d7758f72"
