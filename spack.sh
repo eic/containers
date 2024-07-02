0416bd9789af61f446b058e5a9365e75789b0391
Wouter Deconinck <wdconinc@gmail.com>
2024-07-02 20:15:53 +0000

    fix: rm acts cuda branch commit

--- spack.sh
+++ spack.sh
@@ -12,7 +12,6 @@ read -r -d '' SPACK_CHERRYPICKS <<- \
 f6d50f790ee8b123f7775429f6ca6394170e6de9
 63f6e6079aacc99078386e5c8ff06173841b9595
 9bcc43c4c158639fa6cb575c6106595a34682081
-9f3e45ddbee24aaa7993e575297827e0aed2e6fe
 85f13442d2a7486daba81fdd9a3b6a1182ba11f6
 f73d7d2dce226857cbc774e942454bad2992969e
 cbab451c1a342523ed75e9be1098615a597a9b59
@@ -29,7 +28,6 @@ read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 ## f6d50f790ee8b123f7775429f6ca6394170e6de9: gaudi: Add version 38.1
 ## 63f6e6079aacc99078386e5c8ff06173841b9595: gaudi: upstream patch when @38.1 for missing #include <list>
 ## 9bcc43c4c158639fa6cb575c6106595a34682081: protobuf: update hash for patch needed when="@3.4:3.21"
-## 9f3e45ddbee24aaa7993e575297827e0aed2e6fe: acts: pass cuda_arch to CMAKE_CUDA_ARCHITECTURES
 ## 85f13442d2a7486daba81fdd9a3b6a1182ba11f6: Consolidate concretization output for environments
 ## f73d7d2dce226857cbc774e942454bad2992969e: dd4hep: cleanup recipe, remove deprecated versions and patches
 ## cbab451c1a342523ed75e9be1098615a597a9b59: dd4hep: Add version 1.29
