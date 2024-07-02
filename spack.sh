7c622a00e22d4042c9ac47342649402d57bb1d63
Wouter Deconinck <wdconinc@gmail.com>
2024-07-02 19:18:05 +0000

    chore: spack cherry-pick: acts: add v34.1.0, v35.0.0

--- spack.sh
+++ spack.sh
@@ -16,6 +16,8 @@ f6d50f790ee8b123f7775429f6ca6394170e6de9
 85f13442d2a7486daba81fdd9a3b6a1182ba11f6
 f73d7d2dce226857cbc774e942454bad2992969e
 cbab451c1a342523ed75e9be1098615a597a9b59
+092dc96e6c87a9c043e4421e1a524e23ec649f60
+1dc63dbea6c1d42aabf0e14b51439dcc46423e79
 ---
 ## Optional hash table with comma-separated file list
 read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
@@ -31,3 +33,5 @@ read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 ## 85f13442d2a7486daba81fdd9a3b6a1182ba11f6: Consolidate concretization output for environments
 ## f73d7d2dce226857cbc774e942454bad2992969e: dd4hep: cleanup recipe, remove deprecated versions and patches
 ## cbab451c1a342523ed75e9be1098615a597a9b59: dd4hep: Add version 1.29
+## 092dc96e6c87a9c043e4421e1a524e23ec649f60: acts: pass cuda_arch to CMAKE_CUDA_ARCHITECTURES
+## 1dc63dbea6c1d42aabf0e14b51439dcc46423e79: acts: add v34.1.0, v35.0.0 (identification, sycl variants changes)
