e58b0dab229ff22895b6e10f0b5a4d07cb1a0f71
Wouter Deconinck <wdconinc@gmail.com>
2024-11-05 20:31:12 +0000

    feat: hepmcmerger

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="6aed060d869e3ff825994354226243921a211261"
+EICSPACK_VERSION="dbb544200a6e2b347e732965a6cc6d78f3fb71cc"
