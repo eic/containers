9f1dc2781ba9285bfb5af524e0e940632737d829
Dmitry Kalinkin <dkalinkin@bnl.gov>
2024-12-04 03:37:13 +0000

    feat: epic 24.12.0, eicrecon 1.20.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="97fc58e22597f5c9b5263d14cc73c16324def9bb"
+EICSPACK_VERSION="a8121b60c90ebe9bc65a32471c0ff3b41f74d157"
