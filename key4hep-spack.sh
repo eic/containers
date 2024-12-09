afaef692b705d656443362e2b784ba2b2fecb5e7
Wouter Deconinck <wdconinc@gmail.com>
2024-12-09 01:07:39 +0000

    fix: key4hep-spack: k4actstracking: do not depend on acts +identification

--- key4hep-spack.sh
+++ key4hep-spack.sh
@@ -3,4 +3,4 @@ KEY4HEPSPACK_ORGREPO="key4hep/key4hep-spack"
 
 ## Key4HEP spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-KEY4HEPSPACK_VERSION="d0a5a8d3ed386cd668a31540167d3f1317862cef"
+KEY4HEPSPACK_VERSION="8e13fadcaa76e66efe8807cba3d08170b6272ef3"
