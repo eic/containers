b1f6309956c132ea8796e9ce527ad8d9003f6760
Wouter Deconinck <wdconinc@gmail.com>
2024-12-13 09:53:53 +0000

    feat: eic-spack cherry-pick: dd4hep: add CartesianGridUV  segmentation class,...

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="a8121b60c90ebe9bc65a32471c0ff3b41f74d157"
+EICSPACK_VERSION="739db5b2f9d0a390b7f5b7cbc7293be2a0985a8b"
