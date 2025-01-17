f527972e40487ca2609afcaa45237b4efee44147
Sakib Rahman <rahmans@myumanitoba.ca>
2025-01-17 02:03:17 +0000

    feat: eic-rucio-policy-package to 0.0.6, epic 25.01.1

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="d82cfc842e3a6523d6f29ceecf41641d53c3e4d2"
+EICSPACK_VERSION="72b6ecd18f337e240cef20a886fe6a08da6ccbd6"
