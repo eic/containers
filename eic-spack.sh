97baaa35c7260c441006ff2fbc4c0b5ae13c0d01
Dmitry Kalinkin <dkalinkin@bnl.gov>
2025-02-26 21:23:42 +0000

    feat: bump afterburner to 0.1.3

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="e3b94e80be4351bcb197402d3c8ba66fde9062b7"
+EICSPACK_VERSION="a0a0c6b75bf5821fcbfea3168422529decd59ba3"
