e7c16a09383e4674096bb32d43c08282e63b7787
Sakib Rahman <rahmans@myumanitoba.ca>
2025-03-13 19:25:23 +0000

    Update eic-spack.sh to update rucio-policy-package to 0.0.9

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="ebbfe819d2a56d73b0b405533cc618a9e11a6286"
+EICSPACK_VERSION="b9cbdb470381958f9a0924f62ec45b57ce0efc8a"
