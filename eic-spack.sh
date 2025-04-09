e890a44945a640b3d58d84a631fdb0452db9156d
Wouter Deconinck <wdconinc@gmail.com>
2025-04-09 04:22:14 +0000

    feat: eic-spack: dd4hep: add patch to install plugin headers

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f1ae17322195e5512551b10d17bb438a7fd3835d"
+EICSPACK_VERSION="223a939d3c8abe3d630a6a6fb9aa6e8ae477ca08"
