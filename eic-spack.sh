99c1a47f0e21ff747c44680c2476126087adeb9c
Wouter Deconinck <wdconinc@gmail.com>
2025-04-14 03:12:04 +0000

    chore: eic-spack: juggler: add v15.0.2

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="038a15d38a65247be610e926cb5192ed95217248"
+EICSPACK_VERSION="08b9e2673f14638cdf798c492a6346a96b27c345"
