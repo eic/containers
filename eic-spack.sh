8f1ce15c828dfcb74d9c971558a6d70e62aa1cf9
Wouter Deconinck <wdconinc@gmail.com>
2025-04-29 17:18:41 -0500

    chore: algorithms-1.2.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="0ea4e599b5aea588c67f6bec8b4dc8c1f2942d83"
+EICSPACK_VERSION="bcf09623f502506ce24165824af420ac2a556c9a"
