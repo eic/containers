c71dbafa55b482cb73e3043ded2b8a021663997a
Wouter Deconinck <wdconinc@gmail.com>
2025-04-29 12:29:37 -0500

    fix: eic-spack: eicrecon: patch std::log when 1.24:1.24.2

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="0ea4e599b5aea588c67f6bec8b4dc8c1f2942d83"
+EICSPACK_VERSION="e0bee62888e1e57ab9b8ca177ebae67d8f813fd6"
