fc6c7686369a69e4ad446becc57e1a9d8c726ff6
Wouter Deconinck <wdconinc@gmail.com>
2025-05-06 17:10:30 -0500

    feat: clang-build-analyzer

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="bcf09623f502506ce24165824af420ac2a556c9a"
+EICSPACK_VERSION="6bd21484749467967de7d38e8b7550f7b5807074"
