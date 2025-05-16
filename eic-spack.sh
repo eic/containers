31f6e8d9353393fc1f6d941c44e959ab3e9db8f4
Dmitry Kalinkin <dkalinkin@bnl.gov>
2025-05-16 20:09:34 +0000

    fix: backport TColor threshold patch for DD4hep

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="82b7e423c872c0c242796120208fe1ecb6807ca0"
+EICSPACK_VERSION="6de2f6ced15d9f28361a4e9e2805384fd3144890"
