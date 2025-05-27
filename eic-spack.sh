a539b4b7266bde3e7897f8b446ecd5caa4e33ec8
Wouter Deconinck <wdconinc@gmail.com>
2025-05-27 07:43:39 -0500

    fix: root: patch LookupHelper ROOT lock

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="6de2f6ced15d9f28361a4e9e2805384fd3144890"
+EICSPACK_VERSION="11608ef2b03a168a461b2279b1bb85b6a05f5fbe"
