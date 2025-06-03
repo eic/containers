4c11b606bd2cbc51225ab49bfd499ce1a6629a9c
Wouter Deconinck <wdconinc@gmail.com>
2025-06-03 07:57:34 -0500

    chore: dd4hep-1.32

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="4d88d9f89d9d6f79f645367362758e31974a3d25"
+EICSPACK_VERSION="92081ce329e50d8270b94c15bfd45224d492e369"
