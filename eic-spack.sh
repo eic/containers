e3dc996c9846adbc6c287da24b0a176c626c207c
Wouter Deconinck <wdconinc@gmail.com>
2025-06-08 12:58:11 -0500

    fix: eic-spack: py-rpds-py: rm depends_on rust type=run

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="ea057eb2ead46e2796d427cd084e523ccba672f8"
+EICSPACK_VERSION="8b6346d8880318084dc6335996d0f389880a8b4e"
