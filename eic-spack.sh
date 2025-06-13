9e62fc523c56371943b43e6e08fe6f8cf75c55f3
Wouter Deconinck <wdconinc@gmail.com>
2025-06-13 15:22:05 -0500

    chore: jana2-2.4.2

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="8b6346d8880318084dc6335996d0f389880a8b4e"
+EICSPACK_VERSION="54d0be8f8374ff4e4a10ae430f9d6551a2b55243"
