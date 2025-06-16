a61d9fc2ed48c02a53520d5f5929ebdda20aa65a
Dmitry Kalinkin <dkalinkin@bnl.gov>
2025-06-16 03:46:30 +0000

    feat: hepmcmerger 1.1.1
    (cherry picked from commit 7991bbc63e203f0fd8fb4100764f7889dce1251f)

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="54d0be8f8374ff4e4a10ae430f9d6551a2b55243"
+EICSPACK_VERSION="4d76be7d60889f9453c9208113d7acf779c8f91f"
