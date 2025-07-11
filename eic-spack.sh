8bb56ca6b234d13424c49e18bec597754d1723f3
Wouter Deconinck <wdconinc@gmail.com>
2025-07-11 07:48:14 -0500

    fix: eic-spack: acts: patch to inline ConstPodioTrackStateContainer c'tor

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="008a3bbc826a0b09619ece6327e29d7cdb72ba4b"
+EICSPACK_VERSION="b5d543fa64c6866416033dfe55452348b23b63b0"
