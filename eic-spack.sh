b5b563ad631d5ff7191f87a4c6c5a59220a083e0
Wouter Deconinck <wdconinc@gmail.com>
2025-07-13 19:44:13 -0500

    fix: eic-spack: rivet: variant plugin-match/unmatch

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="dacdda2cf11aa116cecc5803ec62d3052d04c321"
+EICSPACK_VERSION="409e6e8acbbf41da6943afe5dace4be1d307dbc9"
