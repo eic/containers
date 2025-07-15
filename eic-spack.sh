69abd2d93e05897fc6dd4f94844f8a95918a9cca
Dmitry Kalinkin <dkalinkin@bnl.gov>
2025-07-15 23:36:21 +0000

    feat: epic 25.07.0, eicrecon 1.27.1, backport dd4hep signal handler fix

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="409e6e8acbbf41da6943afe5dace4be1d307dbc9"
+EICSPACK_VERSION="50e058b512e237f46fd847fd4c0d31e40048ce30"
