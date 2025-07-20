bc678bbb06262657b7fa7b3d60db9a92bc5494f0
Wouter Deconinck <wdconinc@gmail.com>
2025-07-19 19:48:31 -0500

    fix: eic-spack: acts: fix typo

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f7d537060b93a6d330722e9d16b4931ea9518b30"
+EICSPACK_VERSION="0217f0a4b4ed6d1f421c570f4757ca87fc1296f1"
