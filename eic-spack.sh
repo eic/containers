956eac15920265d65b9941836e43868eb889eea7
Dmitry Kalinkin <dkalinkin@bnl.gov>
2025-07-20 16:05:53 +0000

    fix: update hash eic-spack to be on develop branch

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="0217f0a4b4ed6d1f421c570f4757ca87fc1296f1"
+EICSPACK_VERSION="c2e6c63085bb0eb96f2f3df67d2c4de97eb4cb8b"
