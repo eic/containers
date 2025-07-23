a61b07f07bedd9a1e614e8a4b8ac43b8309621e4
Wouter Deconinck <wdconinc@gmail.com>
2025-07-23 15:26:04 -0500

    chore: eic-spack: dd4hep: add patch for PR#1476

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="44646dcdbf1dae92d228f9b13fa920951b4b19fc"
+EICSPACK_VERSION="3cc68d6679c472a38164da24df7dc7d7df54c58f"
