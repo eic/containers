8aad06ece246ffe09a1895d558f75df25f2fec21
Wouter Deconinck <wdconinc@gmail.com>
2025-08-04 12:47:44 -0500

    feat: use spack v1.0, spack-packages v2025.07.0

---
+++ spack-packages.sh
@@ -0,0 +1,21 @@
+## Spack organization and repository, e.g. spack/spack-packages
+SPACKPACKAGES_ORGREPO="spack/spack-packages"
+
+## Spack github version, e.g. v2025.07.0 or commit hash
+## note: nightly builds will use e.g. develop
+SPACKPACKAGES_VERSION="v2025.07.0"
+
+## Space-separated list of spack-packages cherry-picks
+read -r -d '' SPACKPACKAGES_CHERRYPICKS <<- \
+--- || true
+b5ffbcc4aa43bffbffa96ff9a436a68773e10933
+b6b6d79c25c1496472f9535ec33c9030b27353ab
+---
+## Optional hash table with comma-separated file list
+read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
+--- || true
+---
+## Ref: https://github.com/spack/spack-packages/commit/[hash]
+## [hash]: [description]
+## b5ffbcc4aa43bffbffa96ff9a436a68773e10933: strace: add v6.15
+## b6b6d79c25c1496472f9535ec33c9030b27353ab: iwyu: add patch for 0.23
