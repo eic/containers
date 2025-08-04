20aaffa4772655af3747576d25d46c9c3ba2687d
Wouter Deconinck <wdconinc@gmail.com>
2025-08-04 17:54:30 -0500

    fix: key4hep-spack: fix: ensure from spack.package import *

--- key4hep-spack.sh
+++ key4hep-spack.sh
@@ -3,4 +3,4 @@ KEY4HEPSPACK_ORGREPO="key4hep/key4hep-spack"
 
 ## Key4HEP spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-KEY4HEPSPACK_VERSION="8e13fadcaa76e66efe8807cba3d08170b6272ef3"
+KEY4HEPSPACK_VERSION="5e7831faf74347d1ddb1d20b8a3fbad4d6f04e10"
