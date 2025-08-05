4f13cbb5077f5d3ba03152509a3d318d526e7a2e
Wouter Deconinck <wdconinc@gmail.com>
2025-08-05 18:56:42 -0500

    test: eic-spack: fix: extend v1 and v2 builtin packages

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="7cf490d57606fbd5befe8e20fba1f3c1c6929d86"
+EICSPACK_VERSION="64a1350a25508e250a730c49596aa76cac8b6226"
