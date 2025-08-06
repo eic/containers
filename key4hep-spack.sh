62faeeebaf9bb2ee0c7316813f607791c1fef3b4
Wouter Deconinck <wdconinc@gmail.com>
2025-08-06 14:20:24 -0500

    fix: key4hep-spack: fix: avoid llnl module removal warning in spack v1.0

--- key4hep-spack.sh
+++ key4hep-spack.sh
@@ -3,4 +3,4 @@ KEY4HEPSPACK_ORGREPO="key4hep/key4hep-spack"
 
 ## Key4HEP spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-KEY4HEPSPACK_VERSION="5e7831faf74347d1ddb1d20b8a3fbad4d6f04e10"
+KEY4HEPSPACK_VERSION="2e7a30e56140f8e260f7bff7be5e31784c8e9671"
