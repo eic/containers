44618de82cfd528d3554db321a5a106548b83eb6
Wouter Deconinck <wdconinc@gmail.com>
2025-08-06 12:56:04 -0500

    fix: spack cherry-pick: iwyu,g2,r-curl: requires(pkg) -> requires(^pkg)

--- spack-packages.sh
+++ spack-packages.sh
@@ -10,7 +10,7 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS <<- \
 --- || true
 b5ffbcc4aa43bffbffa96ff9a436a68773e10933
 b6b6d79c25c1496472f9535ec33c9030b27353ab
-5e619a1d482bb5ee059d0a1ec49af1d90e86dee4
+405e97751385dccbd6ec6e6f3b57dc28fc04c76b
 13b4b6e0d9ffc08b12abe9c9b299546b2b382a71
 ---
 ## Optional hash table with comma-separated file list
@@ -21,5 +21,5 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## [hash]: [description]
 ## b5ffbcc4aa43bffbffa96ff9a436a68773e10933: strace: add v6.15
 ## b6b6d79c25c1496472f9535ec33c9030b27353ab: iwyu: add patch for 0.23
-## 5e619a1d482bb5ee059d0a1ec49af1d90e86dee4: iwyu,g2,r-curl: requires(pkg) -> requires(^pkg)
+## 405e97751385dccbd6ec6e6f3b57dc28fc04c76b: iwyu,g2,r-curl: requires(pkg) -> requires(^pkg)
 ## 13b4b6e0d9ffc08b12abe9c9b299546b2b382a71: py-gfal2-python: depends_on c
