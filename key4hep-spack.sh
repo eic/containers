fd26d308047d0e6bea5203496c87fb214e85738f
Wouter Deconinck <wdconinc@gmail.com>
2025-08-07 05:37:43 -0500

    fix: key4hep-spack: k4fwcore,k4actstracking: depends_on c,cxx

--- key4hep-spack.sh
+++ key4hep-spack.sh
@@ -3,4 +3,4 @@ KEY4HEPSPACK_ORGREPO="key4hep/key4hep-spack"
 
 ## Key4HEP spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-KEY4HEPSPACK_VERSION="2e7a30e56140f8e260f7bff7be5e31784c8e9671"
+KEY4HEPSPACK_VERSION="f8fefbd95f543e4d9f1469adf1fc1dacf4252193"
