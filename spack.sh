145681366c3e80703b9c061cf472203992867a60
Wouter Deconinck <wdconinc@gmail.com>
2025-08-06 20:35:39 -0500

    fix: spack: fix: allow versions with git attr in packages without git attr

--- spack.sh
+++ spack.sh
@@ -9,6 +9,7 @@ SPACK_VERSION="v1.0.0"
 read -r -d '' SPACK_CHERRYPICKS <<- \
 --- || true
 09f75ee426a2e05e0543570821582480ff823ba5
+a462612b64e97fa7dfe461c32c58553fd6ec63c5
 ---
 ## Optional hash table with comma-separated file list
 read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
@@ -17,3 +18,4 @@ read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
 ## 09f75ee426a2e05e0543570821582480ff823ba5: setup-env.sh: if exe contains qemu, use /proc/$$/comm instead
+## a462612b64e97fa7dfe461c32c58553fd6ec63c5: fix: allow versions with git attr in packages without git attr
