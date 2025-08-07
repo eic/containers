51b85f36930202229e095c61da29e004c19bb119
Wouter Deconinck <wdconinc@gmail.com>
2025-08-07 05:52:24 -0500

    fix: eic-spack: hepmcmerger,jana2: depends_on c

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="9e4c016bd442e14b43e57e9ae06e7309d839c2f0"
+EICSPACK_VERSION="034ee9f8df549344a2823c269d94ac696a21e8c1"
