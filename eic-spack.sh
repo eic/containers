789ed9a0cc52854f8c4f5557cf8b42e467af8023
Dmitry Kalinkin <dkalinkin@bnl.gov>
2025-08-07 00:32:24 +0000

    Revert "Revert "fix: eic-spack: acts: patches for PRs 4496 and 4502""
    This reverts merge request !1226

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="3cc68d6679c472a38164da24df7dc7d7df54c58f"
+EICSPACK_VERSION="7cf490d57606fbd5befe8e20fba1f3c1c6929d86"
