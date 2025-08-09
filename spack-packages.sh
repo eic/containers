40fa495c6797be1f101551f3760bb0dbd072eab2
Wouter Deconinck <wdconinc@gmail.com>
2025-08-09 09:40:08 -0500

    fix: spack cherry-pick: scikit-hep packages: update to latest major.minor versions

--- spack-packages.sh
+++ spack-packages.sh
@@ -13,6 +13,7 @@ b6b6d79c25c1496472f9535ec33c9030b27353ab
 405e97751385dccbd6ec6e6f3b57dc28fc04c76b
 ba00d764b91db70bce8236bc528a1d4af37c4ce9
 4b243eb07a483a6bf527c2f74e5766b35afa528b
+948d4ea14409e38d47882b5a5c2d61d99d02b30b
 ---
 ## Optional hash table with comma-separated file list
 read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
@@ -25,3 +26,4 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 405e97751385dccbd6ec6e6f3b57dc28fc04c76b: iwyu,g2,r-curl: requires(pkg) -> requires(^pkg)
 ## ba00d764b91db70bce8236bc528a1d4af37c4ce9: py-gfal2-python: depends_on c
 ## 4b243eb07a483a6bf527c2f74e5766b35afa528b: gobject-introspection: restore setuptools@44: support
+## 948d4ea14409e38d47882b5a5c2d61d99d02b30b: scikit-hep packages: update to latest major.minor versions
