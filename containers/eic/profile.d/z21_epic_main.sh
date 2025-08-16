0090c0b94ac94d410795285a72d218fca2c58d2c
Wouter Deconinck <wdconinc@gmail.com>
2025-08-16 17:04:56 -0500

    fix: detect container version

--- containers/eic/profile.d/z21_epic_main.sh
+++ containers/eic/profile.d/z21_epic_main.sh
@@ -2,11 +2,25 @@
 
 # This script auto-loads epic-main configuration iff:
 # - no $DETECTOR_PATH or $DETECTOR_CONFIG is set
-# - /opt/detector/epic-main/bin/thisepic.sh exists
+# - /etc/jug_info contains a line with eic_container
+# - /etc/jug_info contains eic_container: 25.08.0-stable-*
+# - /opt/detector/epic-${version}/bin/thisepic.sh exists
 
-thisepic=/opt/detector/epic-main/bin/thisepic.sh
+file=/etc/jug_info
 if test -z "$DETECTOR_PATH" -a -z "$DETECTOR_CONFIG" ; then
-  if test -f $thisepic ; then
-    source $thisepic
+  if test -f $file ; then
+    version="main"
+
+    eic_container_version=$(sed -nr 's/.*eic_container: (.*)/\1/p' $file)
+    if test -n "$eic_container_version" ; then
+      if [[ $eic_container_version =~ ([0-9]{2}\.[0-9]{2}\.[0-9])-stable-.* ]] ; then
+        version=${BASH_REMATCH[1]}
+      fi
+    fi
+
+    thisepic=/opt/detector/epic-${version}/bin/thisepic.sh
+    if test -f $thisepic ; then
+      source $thisepic
+    fi
   fi
 fi
