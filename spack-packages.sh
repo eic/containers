c4d10d896e43da28a41a7101c82670ba181e0b1a
Wouter Deconinck <wdconinc@gmail.com>
2025-08-27 16:54:11 -0500

    fix: spack cherry-pick: pythia8: add v8.314 and v8.315

--- spack-packages.sh
+++ spack-packages.sh
@@ -17,6 +17,7 @@ ba00d764b91db70bce8236bc528a1d4af37c4ce9
 215e9f93f4de43095bd12e39809b9afeb89655f0
 8aaec9b76104af2cf58e7be55485d6c2385b41ab
 8751ca4c4ba54559b40cdaa3c319bf14db72e28e
+2c1e68ded81add6d3d0fbc005ad19b0727639204
 ---
 ## Optional hash table with comma-separated file list
 read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
@@ -33,3 +34,4 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 215e9f93f4de43095bd12e39809b9afeb89655f0: py-boost-histogram: depends_on py-setuptools-scm type build
 ## 8aaec9b76104af2cf58e7be55485d6c2385b41ab: py-uproot: depends_on py-numpy@:1 when @:5.3.2
 ## 8751ca4c4ba54559b40cdaa3c319bf14db72e28e: py-hist: add v2.6.2 thru v2.8.0 (switch to hatchling)
+## 2c1e68ded81add6d3d0fbc005ad19b0727639204: pythia8: add v8.314 and v8.315
