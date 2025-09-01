8b0d23566d14bcce5dfc9aff48eb83a079d3fa4e
Wouter Deconinck <wdconinc@gmail.com>
2025-08-31 20:14:45 -0500

    fix: spack-packages cherry-pick: py-hist: fix py-boost-histogram dependency

--- spack-packages.sh
+++ spack-packages.sh
@@ -18,6 +18,7 @@ ba00d764b91db70bce8236bc528a1d4af37c4ce9
 8aaec9b76104af2cf58e7be55485d6c2385b41ab
 8751ca4c4ba54559b40cdaa3c319bf14db72e28e
 2c1e68ded81add6d3d0fbc005ad19b0727639204
+b7dbf22114fe746d783f552aa9788dedf58d1769
 ---
 ## Optional hash table with comma-separated file list
 read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
@@ -35,3 +36,4 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 8aaec9b76104af2cf58e7be55485d6c2385b41ab: py-uproot: depends_on py-numpy@:1 when @:5.3.2
 ## 8751ca4c4ba54559b40cdaa3c319bf14db72e28e: py-hist: add v2.6.2 thru v2.8.0 (switch to hatchling)
 ## 2c1e68ded81add6d3d0fbc005ad19b0727639204: pythia8: add v8.314 and v8.315
+## b7dbf22114fe746d783f552aa9788dedf58d1769: py-hist: fix py-boost-histogram dependency
