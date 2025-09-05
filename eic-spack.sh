a3e520847a00aad8fe259b545b06d5245c5e1985
Wouter Deconinck <wdconinc@gmail.com>
2025-09-05 10:27:06 -0500

    Revert "test: eic-spack: root: patch to prevent nullptr access in Restart"
    This reverts commit 0313b2fe3a5203b908d621663cc929ca2323ba9d.

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="treeplayer-prevent-nullptr-access-in-Restart"
+EICSPACK_VERSION="4d384847b3b7c7ac3cc8d797ba46a021ffaef448"
