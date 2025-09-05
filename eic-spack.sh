0aa1e1cf13aaf50fc0d88e834cb1b076ab798356
Wouter Deconinck <wdconinc@gmail.com>
2025-09-05 13:46:44 -0500

    fix: eic-spack: dd4hep: patch to avoid double deduce/open for HEPMC3FileReader when not ReaderAscii

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="4d384847b3b7c7ac3cc8d797ba46a021ffaef448"
+EICSPACK_VERSION="dd4hep-pr1495"
