b01fda486c670110fdeadc78272d57b28d4abf05
Wouter Deconinck <wdconinc@gmail.com>
2025-09-06 22:37:02 -0500

    fix: acts +pr4620

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="801664489056f26a51f6c54e65e1852c1295ef41"
+EICSPACK_VERSION="c94153b9fdebec89c4b7816fe66e6deddd097dd1"
