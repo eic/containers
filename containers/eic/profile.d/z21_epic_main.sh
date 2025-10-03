060dc6a48b4d4471c66535cbc6dd91b08cc1433c
Sakib Rahman <rahmans@myumanitoba.ca>
2025-10-03 16:27:23 +0000

    Make z21_epic_main.sh dash compliant

--- containers/eic/profile.d/z21_epic_main.sh
+++ containers/eic/profile.d/z21_epic_main.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-
 # This script auto-loads epic-main configuration iff:
 # - no $DETECTOR_PATH or $DETECTOR_CONFIG is set
 # - /etc/jug_info contains a line with jug_.*
 # - /etc/jug_info contains version info: 25.08.0-stable-*
 # - /opt/detector/epic-${version}/bin/thisepic.sh exists
-
 file=/etc/jug_info
 if test -z "$DETECTOR_PATH" -a -z "$DETECTOR_CONFIG" ; then
-  if test -f $file ; then
+  if test -f "$file" ; then
     version="main"
-
-    eic_container_version=$(sed -nr 's/.*jug_.*: (.*)/\1/p' $file)
+    eic_container_version=$(sed -n 's/.*jug_.*: \(.*\)/\1/p' "$file")
     if test -n "$eic_container_version" ; then
-      if [[ $eic_container_version =~ ([0-9]{2}\.[0-9]{2}\.[0-9])-stable-.* ]] ; then
-        version=${BASH_REMATCH[1]}
+      # Extract version using sed with basic regex (POSIX)
+      extracted_version=$(echo "$eic_container_version" | sed -n 's/^\([0-9]\{2\}\.[0-9]\{2\}\.[0-9]\)-stable-.*/\1/p')
+      if test -n "$extracted_version" ; then
+        version="$extracted_version"
       fi
     fi
-
     thisepic=/opt/detector/epic-${version}/bin/thisepic.sh
-    if test -f $thisepic ; then
-      source $thisepic
+    if test -f "$thisepic" ; then
+      . "$thisepic"
     fi
   fi
 fi
