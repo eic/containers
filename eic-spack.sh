892e0b72770307439a4dc654709fd1fa5b8e5c27
Dmitry Kalinkin <dkalinkin1@bnl.gov>
2025-10-08 20:33:57 +0000

    feat: edm4eic 8.3.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="29cbba9068d41486c2a6d5c46086bd73b32e6caa"
+EICSPACK_VERSION="6034a8c7479abb39a77436352715dfba48d4447d"
