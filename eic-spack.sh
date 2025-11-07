b3fbc1f385381d25062e8a6e854a54f5aba1be1c
Dmitry Kalinkin <dkalinkin1@bnl.gov>
2025-11-07 18:09:12 +0000

    feat: add irt2

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="4fd01456e3a672310fe5f154594cc3ea58ce2e58"
+EICSPACK_VERSION="b30012e2d0dc42f4b553537bd11209b1785ebbd5" # FIXME dangling irt2 PR
