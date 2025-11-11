21d2c439faac36406a6963ab29c32cd68777857a
Dmitry Kalinkin <dkalinkin1@bnl.gov>
2025-11-11 19:50:16 +0000

    Update 3 files
    - /eic-spack.sh
    - /spack-environment/xl/spack.yaml
    - /spack-environment/cuda/spack.yaml

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="85bc9c63aca5aa74ac7da771701e136656eacb8a"
+EICSPACK_VERSION="3bcc41383d23fc054b5cf56da9c8c43d7408897a"
