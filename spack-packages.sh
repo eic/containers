528df5f5814ca66c1e0e4cbd29c95c253f5d0dee
Wouter Deconinck <wdconinc@gmail.com>
2025-11-20 18:36:39 -0600

    fix: rm spack cherry-pick: actsvg: patch version numbers into source source

--- spack-packages.sh
+++ spack-packages.sh
@@ -13,7 +13,6 @@ a115a811bdfce4db5298a9ba9b7903ccfb0de101
 559789f67245a40306aeea636b61348e97d4f092
 95c45b6c3322e151fd29ed00ea10567b97705ee4
 d6f99b8d611cf740eb45e13df2c84044a4ca6ae4
-bc25e5eb2f8f7a8733cdc10d92e9787358c82cfa
 5c37f836753e8f9683fdc547f3661c5045abcbd1
 ---
 ## Optional hash table with comma-separated file list
@@ -27,5 +26,4 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 559789f67245a40306aeea636b61348e97d4f092: estarlight: add thru v1.2.0
 ## 95c45b6c3322e151fd29ed00ea10567b97705ee4: py-tensorflow: modify cuDNN dependency versions when +cuda
 ## d6f99b8d611cf740eb45e13df2c84044a4ca6ae4: py-tensorflow: patch to build with +cuda
-## bc25e5eb2f8f7a8733cdc10d92e9787358c82cfa: actsvg: patch version numbers into source source
 ## 5c37f836753e8f9683fdc547f3661c5045abcbd1: root: require openblas ~ilp64 symbol_suffix=none when ^openblas
