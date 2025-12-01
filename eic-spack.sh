d3975d41341b00daf19ca0e9e996376d7a84a464
Wouter Deconinck <wdconinc@gmail.com>
2025-12-01 16:10:27 -0600

    chore: podio-1.6, jana2-2.4.3 (#77)
    * chore: podio-1.6
    
    * fix: spack cherry-pick: Deprecation removals: P (limited to podio)
    
    * chore: jana2-2.4.3

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="2ac6537435128a2dae054ab14a0bbf23f4fb0fb1"
+EICSPACK_VERSION="f4236f3c2edd6bfc8e0d6dfcdf3e0d0ac6dc5b0b"
