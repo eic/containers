39d51a7d0dbf535d631efdf79f2fbdea09b38970
Wouter Deconinck <wdconinc@gmail.com>
2025-12-06 07:39:02 -0600

    chore: acts-44.3.0 (#80)
    * chore: acts-44.3.0
    
    * chore: eigen-3.4.1
    
    * fix: eic-spack: juggler: add v15.0.4
    
    * chore: juggler-15.0.4
    
    * fix: eic-spack: juggler: add v15.0.5
    
    * chore: juggler-15.0.5

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="0441d92df3c7f2563532d78078e3ca0588e9fb1f"
+EICSPACK_VERSION="c95a1e1f5da006072e9f8a6f4e7c8bbe072b2143"
