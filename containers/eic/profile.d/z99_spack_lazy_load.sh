edcd4dd79d251219f6681def91af228aa9a426c7
Wouter Deconinck <wdconinc@gmail.com>
2026-01-15 14:38:21 -0600

    Revert "feat: add alias for spack in z99_spack_lazy_load (#130)"
    This reverts commit 201ca937da06d88a32f28d727b2f98d900c33be9.

--- containers/eic/profile.d/z99_spack_lazy_load.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Create alias for first invocation of spack
-# that loads environment to avoid slow loads
-# wben sourcing environment by default
-alias spack='unalias spack && source ${SPACK_ROOT}/share/spack/setup-env.sh && spack'
