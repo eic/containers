ac23e71ab1812924906089bf35af6960c8bd32dc
Wouter Deconinck <wdconinc@gmail.com>
2026-01-15 20:18:25 -0600

    Revert "feat: jana2 2026.01.01 (#122)"
    This reverts commit a9a8bfe97dd8414dfffc154c9701047ff381fd8f.

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="6fcf84983bfb9fc3d63e9540915001ef03ff5dbf"
+EICSPACK_VERSION="640379e054350a37f3336b5239f8661398ebb825"
