a66e77cc7bde4e7e3feba4bf4242923edacb04f9
Wouter Deconinck <wdconinc@gmail.com>
2026-01-18 16:07:11 -0600

    chore: edm4eic-8.6.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="6fcf84983bfb9fc3d63e9540915001ef03ff5dbf"
+EICSPACK_VERSION="b63d5612c41b29c1a65b5607b544f1766c323bf4"
