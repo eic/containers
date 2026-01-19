18bad8f9f27fd81a380c95ffb1203958e546ac15
Wouter Deconinck <wdconinc@gmail.com>
2026-01-19 10:13:09 -0600

    chore: acts@44.4.0 +edm4hep (#129)
    * fix: acts +edm4hep
    
    * fix: spack cherry-pick: acts: narrow when range on podio when +edm4hep +examples
    
    * chore: acts-44.4.0
    
    * fix: eic-spack: acts: patch for PR 4968, fix: Use (cached) runtime type hash in Acts::Any

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="b63d5612c41b29c1a65b5607b544f1766c323bf4"
+EICSPACK_VERSION="8e231cc01baa7b4cdc285671e5fa4e92d2d35547"
