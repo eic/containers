d7ae0389860b7777df9b016d79c7464de7a10238
Wouter Deconinck <wdconinc@gmail.com>
2026-01-18 18:47:58 -0600

    fix: eic-spack: acts: patch for PR 4968, fix: Use (cached) runtime type hash in Acts::Any

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="640379e054350a37f3336b5239f8661398ebb825"
+EICSPACK_VERSION="8e231cc01baa7b4cdc285671e5fa4e92d2d35547"
