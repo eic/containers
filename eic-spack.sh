8f0b1e413e05de3257eb8ee700b2077c2ee82626
Wouter Deconinck <wdconinc@gmail.com>
2026-01-21 18:08:18 -0600

    fix: eic-spack: geant4: add v11.4.0.east

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="8e231cc01baa7b4cdc285671e5fa4e92d2d35547"
+EICSPACK_VERSION="f16193d67aa36aab9c773590f5998ae47d9ba4b0"
