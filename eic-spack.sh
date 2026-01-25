ab148abdb04f0520152f98ab9c52508836ec5bfc
Dmitry Kalinkin <dkalinkin@bnl.gov>
2026-01-25 18:58:24 -0500

    acts: patch to skip propagation erros for material map generation

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="0697076ae087863cf87e48acfc1457fa40fda478"
+EICSPACK_VERSION="3a2e64ceab3878c0c7ccdf005f20588a5d08167d"
