18d61fcee785ae90fda671c0f6ba91629f22017f
Wouter Deconinck <wdconinc@gmail.com>
2026-02-02 20:41:09 -0600

    chore: eic-smear-1.1.16 (#148)

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="36a4e51ace5f74d4dc06033bc1f573350a91a2b2"
+EICSPACK_VERSION="9f7ef015437f210c46122a3ff4de438cc35e3202"
