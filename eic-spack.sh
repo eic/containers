64b39ac54d1674e43ee91e9f02ffb235e7647e34
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-02-04 22:19:48 -0500

    feat: add eic-opticks to eic_cuda

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f07e969995563a11ca81138b4489b090c08ac71a"
+EICSPACK_VERSION="64086c55652c0b0963372dd0902bc4d41982956c" # FIXME once eic-spack merged
