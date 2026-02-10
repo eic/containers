79f096a2cd195d3a0260c7baf8c7facdde067f30
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-02-10 14:27:13 -0500

    feat: irt 2.1.1

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="7684c9f57df649ed48091d3da2d66a4fd03be17a"
+EICSPACK_VERSION="0beb14f8953ab420e0aa4207462f529d8c1a60ec" # FIXME
