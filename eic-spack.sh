30151484eb290fa9cce4955cf39c534466309e98
Wouter Deconinck <wdconinc@gmail.com>
2026-02-11 08:10:32 -0600

    fix: eic-spack: jana2: patch for PR#464

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="fca34881013ac214b0a880347792fd92e42407a8"
+EICSPACK_VERSION="b652988115c4c8a23392cc7ad5797e7e87f36493"
