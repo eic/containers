f28cd4c98f5901db8e8eda62ac5fd65926f68698
Dmitry Kalinkin <dkalinkin@bnl.gov>
2026-02-14 14:31:27 -0500

    feat: build epic with irt2 support (if available)

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="87ef65949ddd47fccee52342996f273fa16ed17d"
+EICSPACK_VERSION="51525dbeb5ca26b83e10f66aca767ff114ec9229"
