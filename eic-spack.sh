8302362191ed5d5ad49cc42f42ff7c69409f12cc
Wouter Deconinck <wdconinc@gmail.com>
2026-02-18 07:37:13 -0600

    fix: eic-spack: py-uproot: backport RNTuple v1.0.1.0 format support

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="3ac992dd4ce49164780d1496c23d837bd58290d1"
+EICSPACK_VERSION="26229254caba2ce95a5ce3741d33ee59aa252633"
