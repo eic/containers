fc5d777ffacc2bc20cefd8a9b95785ebfc616884
Wouter Deconinck <wdconinc@gmail.com>
2026-02-22 10:00:29 -0600

    Revert "fix: remove cherry-pick in new release"
    This reverts commit 0d38b98bdae836f10a5225c34973bb5182c0c72a.

--- spack.sh
+++ spack.sh
@@ -9,6 +9,7 @@ SPACK_VERSION="v1.0.3"
 read -r -d '' SPACK_CHERRYPICKS <<- \
 --- || true
 09f75ee426a2e05e0543570821582480ff823ba5
+a462612b64e97fa7dfe461c32c58553fd6ec63c5
 292b0dcaba3b2a5e3f9668d205d39fee2c715721
 ---
 ## Optional hash table with comma-separated file list
@@ -18,4 +19,5 @@ read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
 ## 09f75ee426a2e05e0543570821582480ff823ba5: setup-env.sh: if exe contains qemu, use /proc/$$/comm instead
+## a462612b64e97fa7dfe461c32c58553fd6ec63c5: fix: allow versions with git attr in packages without git attr
 ## 292b0dcaba3b2a5e3f9668d205d39fee2c715721: fix: write created time field with OCI buildcache config
