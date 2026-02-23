0e36139a84caf3689c5a2443503172df579ff8dd
Wouter Deconinck <wdconinc@gmail.com>
2026-02-22 22:25:24 -0600

    chore: spack-1.0.3 (#173)
    * chore: spack-1.0.3
    
    * fix: remove cherry-pick in new release
    
    Removed a cherry-pick reference for a specific commit.

--- spack.sh
+++ spack.sh
@@ -3,7 +3,7 @@ SPACK_ORGREPO="spack/spack"
 
 ## Spack github version, e.g. v0.18.1 or commit hash
 ## note: nightly builds will use e.g. releases/v1.0
-SPACK_VERSION="v1.0.2"
+SPACK_VERSION="v1.0.3"
 
 ## Space-separated list of spack cherry-picks
 read -r -d '' SPACK_CHERRYPICKS <<- \
