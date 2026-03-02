d0722161a58fd8cbbeb55efe33b519ed1b0a8922
Dmitry Kalinkin <dkalinkin@bnl.gov>
2026-03-02 11:02:28 -0500

    feat: irt2 2.1.3 (#177)

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="42be3da82b02e63350418707b89e881e83c671bc"
+EICSPACK_VERSION="2a0b2e4002346624c8b2504a714be9ae4898e5ae"
