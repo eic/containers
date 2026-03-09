8879cee610ea259ea4f44e0e0157672b1072f1f4
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-03-09 19:18:13 -0400

    fix: workaround jana2 issue with jana2_tutorial_podio

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="c6c0b369542bf60e79f684fb4a37f8c766dff230"
+EICSPACK_VERSION="7f6ce6379e29f6154a67f68c56ede4b9bc96b558" # FIXME
