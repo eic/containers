f8a97e18671dbd5b5496b9422f8750d4bda42cce
Wouter Deconinck <wdconinc@gmail.com>
2026-03-09 13:24:42 -0500

    fix: eic-spack: py-uproot: Add patch for uproot RNTuple _from_zigzag unsigned integer fix

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="c6c0b369542bf60e79f684fb4a37f8c766dff230"
+EICSPACK_VERSION="cd0487fbc78717c298c1efe2b8fa7026366ffcc6"
