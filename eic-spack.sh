83a1493d8e0166afd4ddaac5b358fc33ea7121d4
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-03-09 10:24:27 -0400

    feat: eic-opticks 0.1.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="c6c0b369542bf60e79f684fb4a37f8c766dff230"
+EICSPACK_VERSION="f9b9adfa778645d6aa5f53d72d32d9c80d11d8e6" # FIXME
