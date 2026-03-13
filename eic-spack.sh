9c7b67135df7d12fb918f413044d9789e8c7fa63
Wouter Deconinck <wdconinc@gmail.com>
2026-03-13 14:49:11 -0500

    chore: eicrecon-1.35.2; epic-26.03.1 (#205)

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="137a18b95edd709aecd28025356f3da4810b4348"
+EICSPACK_VERSION="d8cb0800fb687791004594441f2b1226fb1a4eee"
