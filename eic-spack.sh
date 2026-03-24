138e86c5035615b37fd3a9a0950f41cadebab905
Dmitry Kalinkin <dkalinkin@bnl.gov>
2026-03-24 15:05:35 -0400

    Update eic-spack.sh

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="d8cb0800fb687791004594441f2b1226fb1a4eee"
+EICSPACK_VERSION="5f805b5e9be1ca2d24382f1e152c24f924846f19" # FIXME use commit on main branch once downstream PR is merged
