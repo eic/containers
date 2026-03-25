f4cb94bbb43695d1c0b6bf7baca2d09cb4e9e74c
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-03-25 11:56:00 -0400

    feat: afterburner 0.2.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f09407ea393e26ecff50b03a5a26127929fe7909"
+EICSPACK_VERSION="cf34155b740f7761270d8338aa8d4e07e4bc51d0"
