b714c0ea3cc376a69da4bc4ffd215f003b91103e
Dmitry Kalinkin <dkalinkin@bnl.gov>
2026-03-25 10:16:09 -0400

    feat: dd4hep patch to add random photon polarization (#209)

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="d8cb0800fb687791004594441f2b1226fb1a4eee"
+EICSPACK_VERSION="f09407ea393e26ecff50b03a5a26127929fe7909"
