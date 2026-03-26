f1ba18b3c5cb57a158c3b2806f5ee9c6a218f71c
Dmitry Kalinkin <dkalinkin@bnl.gov>
2026-03-26 19:27:12 -0400

    Update eic-spack.sh

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="7433d9a42937d15f7b59b017f40fed61529a4f93" # FIXME
+EICSPACK_VERSION="aec95222f3b820666a1f5570e7141f75c1a14e8b" # FIXME
