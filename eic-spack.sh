221b0f0740d3501f04b02e5413c0d35eea2c46a4
Wouter Deconinck <wdconinc@gmail.com>
2026-04-05 11:54:49 -0500

    fix: eic-spack: ix: py-tensorflow: run_before build

--- eic-spack.sh
+++ eic-spack.sh
@@ -3,4 +3,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="babd48646daabd886d0be2c2a99bc0658d1370d6"
+EICSPACK_VERSION="c7fbd1a14396e5e1de529a75e21e56889b9d8fb3"
