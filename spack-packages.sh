f11a819fb5638be32510f8471a746cfd1a4f0a12
Wouter Deconinck <wdconinc@gmail.com>
2026-04-13 12:03:39 -0500

    fix: spack cherry-pick: vecgeom: add v2.0.0

--- spack-packages.sh
+++ spack-packages.sh
@@ -51,6 +51,8 @@ cfa8d650480c409de2d568cf1355bf7e509f4c1c
 b2f469de6a7c823bf93b9fa408b0ef4b67442f5f
 3368f8849e57da0ad1eaa0086b1855538e5db03e
 5ec7462a628681522f8cf6016a9aa72fbd40dd91
+a896fdbe5d01981cbc6f9b5139a5d551ac2fe248
+2163a6ae2cad38980f67e8d617743cabfc773304
 0d4e42deee0f561013568d1e92205a084cf203bc
 6346a54e35ee6281b8e8c6a1bc9c102893593c8e
 ---
@@ -102,5 +104,7 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## b2f469de6a7c823bf93b9fa408b0ef4b67442f5f: py-snakemake-storage-plugin-rucio: add v0.4.1
 ## 3368f8849e57da0ad1eaa0086b1855538e5db03e: just: add 1.46.0
 ## 5ec7462a628681522f8cf6016a9aa72fbd40dd91: arrow: add 20.0.0, 21.0.0, 22.0.0
+## a896fdbe5d01981cbc6f9b5139a5d551ac2fe248: vecgeom: new release 2.0.0-rc.9
+## 2163a6ae2cad38980f67e8d617743cabfc773304: vecgeom: add v2.0.0
 ## 0d4e42deee0f561013568d1e92205a084cf203bc: g4hepem: new package
 ## 6346a54e35ee6281b8e8c6a1bc9c102893593c8e: g4adept: new package
