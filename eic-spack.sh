99079dc426692ac0314d16019e76e951f6212a27
Dmitry Kalinkin <dkalinkin@bnl.gov>
2026-04-16 11:11:39 -0400

    fix: remove LD_LIBRARY_PATH from epic's setup_run_environment

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="444a3e11a1fd5ad46d04231c94b08264444acf2c"
+EICSPACK_VERSION="3dc9e76edf4aad8cc6f377d320f3cead81e1ecae"
