a65a4e733a72f81d3ab884da981648740cdeb26a
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-04-27 20:48:35 -0400

    bump eic-spack, npsim: 1.5.0

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="582258cf6648228953cfafdb843ed36782970fab" # FIXME
+EICSPACK_VERSION="82b78c8bc281005289bb45ffcfeb78162c4eced8"
