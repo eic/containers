a15df0a5ea60ccd5105f87a7befaf4a614ec4215
Wouter Deconinck <wdconinc@gmail.com>
2026-05-08 12:29:25 -0500

    fix: eic-spack: epic-generator: mv to the final resting place

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="383a2c5f18b41ca86f1f3474a92254666942b11e"
+EICSPACK_VERSION="95729ec8cd982271a6f96eb5c7f72c90a3316720"
