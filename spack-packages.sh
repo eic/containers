f89ca5d09b5a67ceea287ff87b1ff47340bb7177
Wouter Deconinck <wdconinc@gmail.com>
2026-05-11 10:42:19 -0500

    feat: eic_{ci,xl}: add pelican ~server

--- spack-packages.sh
+++ spack-packages.sh
@@ -23,6 +23,7 @@ bd32638d817fa66188880e557196acd2a5d6e7b6
 b17f3abec760256ad7faf1b1d102f2553d6a3622
 45f50975f023b3a444217e3d474c2b2d264763b3
 6b86b44735775c97636861dd66ee14ce83906422
+7f9f7ff5c232068aec8ff30b147a098dd25d9ef9
 ---
 ## Optional hash table with comma-separated file list
 ## For these commits, the cherry-pick will be restricted to the listed files only.
@@ -43,3 +44,4 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## b17f3abec760256ad7faf1b1d102f2553d6a3622: (py-)protobuf: add v(5.)28.3
 ## 45f50975f023b3a444217e3d474c2b2d264763b3: py-torch: patch for GCC-14.2 ICE on aarch64
 ## 6b86b44735775c97636861dd66ee14ce83906422: apfelxx, epic, partons*, sfml: new packages
+## 7f9f7ff5c232068aec8ff30b147a098dd25d9ef9: pelican: new package, v7.24.3
