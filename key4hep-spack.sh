f8196ecd3d1fd34d6542f001a0a191f189b077f3
Wouter Deconinck <wdconinc@gmail.com>
2026-05-11 22:42:33 -0500

    fix: key4hep-spack: k4actstracking: don't depend on acts +tgeo

--- key4hep-spack.sh
+++ key4hep-spack.sh
@@ -5,4 +5,4 @@ KEY4HEPSPACK_ORGREPO="key4hep/key4hep-spack"
 
 ## Key4HEP spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-KEY4HEPSPACK_VERSION="d4292f5042c47b4b19dccf98fb8a28b58143537e"
+KEY4HEPSPACK_VERSION="c53548c3ee19cac56017c5d096aaf5b015d0e5a6"
