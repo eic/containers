815371ca8fa8126e64f2cdf013eca1636db9b3d0
Wouter Deconinck <wdconinc@gmail.com>
2026-05-17 09:15:54 -0500

    fix: eic-spack: dd4hep: add v1.36-mt.1 and v1.37-mt.1

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="4f554f275a713dc35af3cdec3968ada2c834aa18"
+EICSPACK_VERSION="60be4761de4bd775b825edcf5a667ad84ebc1685"
