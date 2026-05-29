f9fcb59b3acac48acf860a3f3bc0c9a29876b04b
Wouter Deconinck <wdconinc@gmail.com>
2026-05-29 17:57:35 -0500

    fix: eic-spack: feat(hwcaps): require glibc and x86_64 target when hwcaps is active

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="e205f238ccca722322edb6786f0716ab9697a411"
+EICSPACK_VERSION="84978bfb1ba00f2da4d86f7960a88ccbcb242eff"
