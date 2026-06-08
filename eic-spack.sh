24aaa4a573371ef5c34eaecba2a8cab7a58a757c
Dmitry Kalinkin <dkalinkin1@bnl.gov>
2026-06-08 09:23:25 -0400

    feat: epic 26.06.0, eicrecon 1.38.0, npsim 1.6.0 with C++20 (#305)
    * feat: epic 26.06.0, eicrecon 1.38.0, npsim 1.6.0
    
    * feat: compile npsim with C++20

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f56f48eeb7c4b2c286962ab4d7048d821bc35c6f"
+EICSPACK_VERSION="9cb75284f136ce8cf9bc54fe5b979e40e6eba485"
