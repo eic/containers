55d4ef507ae3bf69ef3601b39827997e7f6c962e
Wouter Deconinck <wdconinc@gmail.com>
2026-06-20 17:59:07 -0500

    feat: eic-spack: dd4hep: add g4hepem patch

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="8d090a161331ff5e286ee094eb65adfa35aa6225"
+EICSPACK_VERSION="f081c3170ae3b3f0e4867eb499c48aba41895d73"
