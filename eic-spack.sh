4bfab0d81eb5e61c9f2f6ee7dcf3a55275532ff2
Wouter Deconinck <wdconinc@gmail.com>
2026-06-21 20:49:03 -0500

    fix: eic-spack: pyrobird: yank 0.2.7

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="8d090a161331ff5e286ee094eb65adfa35aa6225"
+EICSPACK_VERSION="f51626b3e93b5256053e2aa9b617629e43ff375f"
