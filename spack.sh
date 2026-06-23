dd1eecb7e4821d4b7a8b4ae881a7a0b379322f28
Wouter Deconinck <wdconinc@gmail.com>
2026-06-23 09:32:20 -0500

    fix: spack cherry-pick: view: fix rename on overlayfs issue

--- spack.sh
+++ spack.sh
@@ -13,6 +13,7 @@ read -r -d '' SPACK_CHERRYPICKS <<- \
 292b0dcaba3b2a5e3f9668d205d39fee2c715721
 678e506a95b319c573ba7e84703b06d7275ab80e
 52bef0e63711a0b63816b3f4b1561df5b158db65
+44fa40a0367ab59f756fc24791395e1ec56d2712
 ---
 ## Optional hash table with comma-separated file list
 read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
@@ -23,3 +24,4 @@ read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 ## 292b0dcaba3b2a5e3f9668d205d39fee2c715721: fix: write created time field with OCI buildcache config
 ## 678e506a95b319c573ba7e84703b06d7275ab80e: fix: don't map prefix to view root for pkgs excluded from view
 ## 52bef0e63711a0b63816b3f4b1561df5b158db65: fix: install missing upstream packages in local store
+## 44fa40a0367ab59f756fc24791395e1ec56d2712: view: fix rename on overlayfs issue
