02563b620894d39a34e00fd99f36abfe20e25139
Wouter Deconinck <wdconinc@gmail.com>
2026-06-28 09:52:58 -0500

    fix: spack cherry-pick: fix: install missing upstream packages in local store (new installer)

--- spack.sh
+++ spack.sh
@@ -12,7 +12,7 @@ read -r -d '' SPACK_CHERRYPICKS <<- \
 --- || true
 292b0dcaba3b2a5e3f9668d205d39fee2c715721
 678e506a95b319c573ba7e84703b06d7275ab80e
-52bef0e63711a0b63816b3f4b1561df5b158db65
+e3947b8556adc9ed0ff42785ac920a38d2435dd9
 44fa40a0367ab59f756fc24791395e1ec56d2712
 ---
 ## Optional hash table with comma-separated file list
@@ -23,5 +23,5 @@ read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 ## [hash]: [description]
 ## 292b0dcaba3b2a5e3f9668d205d39fee2c715721: fix: write created time field with OCI buildcache config
 ## 678e506a95b319c573ba7e84703b06d7275ab80e: fix: don't map prefix to view root for pkgs excluded from view
-## 52bef0e63711a0b63816b3f4b1561df5b158db65: fix: install missing upstream packages in local store
+## e3947b8556adc9ed0ff42785ac920a38d2435dd9: fix: install missing upstream packages in local store
 ## 44fa40a0367ab59f756fc24791395e1ec56d2712: view: fix rename on overlayfs issue
