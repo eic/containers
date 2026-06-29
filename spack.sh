f5770e4eb96464a5d20611e91bcf7c1f3a576662
Wouter Deconinck <wdconinc@gmail.com>
2026-06-29 12:49:04 -0500

    fix: spack cherry-pick: fix: also avoid raise InstallError later in new_installer; add unit-test

--- spack.sh
+++ spack.sh
@@ -12,8 +12,9 @@ read -r -d '' SPACK_CHERRYPICKS <<- \
 --- || true
 292b0dcaba3b2a5e3f9668d205d39fee2c715721
 678e506a95b319c573ba7e84703b06d7275ab80e
-68e3807eab8e1499306e43119aa3d4bc4a0e90ec
-e3947b8556adc9ed0ff42785ac920a38d2435dd9
+2b588bdf631a22a55630981aa69365197a9258f7
+2ec34f3a913fe32017682ccbc9518b7111255f6b
+69329c85739c6030d6f60c00aa03ebcaf4e22037
 44fa40a0367ab59f756fc24791395e1ec56d2712
 ---
 ## Optional hash table with comma-separated file list
@@ -24,6 +25,7 @@ read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 ## [hash]: [description]
 ## 292b0dcaba3b2a5e3f9668d205d39fee2c715721: fix: write created time field with OCI buildcache config
 ## 678e506a95b319c573ba7e84703b06d7275ab80e: fix: don't map prefix to view root for pkgs excluded from view
-## 68e3807eab8e1499306e43119aa3d4bc4a0e90ec: fix: install missing upstream packages in local store
-## e3947b8556adc9ed0ff42785ac920a38d2435dd9: fix: install missing upstream packages in local store (new installer)
+## 2b588bdf631a22a55630981aa69365197a9258f7: fix: install missing upstream packages in local store
+## 2ec34f3a913fe32017682ccbc9518b7111255f6b: fix: install missing upstream packages in local store (new installer)
+## 69329c85739c6030d6f60c00aa03ebcaf4e22037: fix: also avoid raise InstallError later in new_installer; add unit-test
 ## 44fa40a0367ab59f756fc24791395e1ec56d2712: view: fix rename on overlayfs issue
