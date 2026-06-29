335cadce23ee0b47a754e96110b129f28c50c131
Wouter Deconinck <wdconinc@gmail.com>
2026-06-29 17:46:56 -0500

    fix: spack cherry-pick: py-boto3: add v1.43.17, updated dependencies

--- spack-packages.sh
+++ spack-packages.sh
@@ -34,6 +34,9 @@ deb4f17d93dbe012403614245334f7c73fcc086f
 0d64b2bab72a99441f042b663e4bc993ec0db45d
 a77a7ed0d2630466cac71165026387b1381b058a
 403e4e0b2189600a736106bcfee568e31b5bcb22
+88800eeac4ee97b7689b0a84d066e4f634f32b46
+d8271eec35998674d3c67c7613b010fa98519df3
+86e2acd247221d45f9944ac2f69518b8766baec3
 ---
 ## Optional hash table with comma-separated file list
 ## For these commits, the cherry-pick will be restricted to the listed files only.
@@ -65,3 +68,6 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 0d64b2bab72a99441f042b663e4bc993ec0db45d: osg-ca-certs: depends on gmake and perl, type build
 ## a77a7ed0d2630466cac71165026387b1381b058a: py-pynacl: depends on gmake, type build
 ## 403e4e0b2189600a736106bcfee568e31b5bcb22: py-rucio-clients: add 40.2.0
+## 88800eeac4ee97b7689b0a84d066e4f634f32b46: py-boto3: add v1.42.85 and py-botocore: add v1.42.85
+## d8271eec35998674d3c67c7613b010fa98519df3: py-setuptools-scm: Earlier versions require setuptools-scm too
+## 86e2acd247221d45f9944ac2f69518b8766baec3: py-boto3: add v1.43.17, updated dependencies
