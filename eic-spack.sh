1097346a6589aef34cb6169032405abaf2d48941
Dmitry Kalinkin <dkalinkin1@bnl.gov>
2026-06-30 13:34:27 -0400

    feat: symphony 0.7.0 (#326)

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="ec7e55c3776da9923b7c68a4ddb9a2dfff5aaf18"
+EICSPACK_VERSION="01aaad210b55164d6c249fed94f040d9272c6f39"
