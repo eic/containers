c2659d835b42e9b6e09e952013075ff8360cf844
Wouter Deconinck <wdconinc@gmail.com>
2026-07-01 13:32:05 -0500

    fix: use develop branch commits in cherry-picks

--- spack-packages.sh
+++ spack-packages.sh
@@ -16,10 +16,9 @@ a115a811bdfce4db5298a9ba9b7903ccfb0de101
 20444b8e9382e659360a1446688d10a8c2d2ad31
 f5742718da7bd1d078ddc8423011a82ef2e3c759
 deb4f17d93dbe012403614245334f7c73fcc086f
-0d64b2bab72a99441f042b663e4bc993ec0db45d
-a77a7ed0d2630466cac71165026387b1381b058a
-bb6cd78da2a0c213508820fd27e322f5582e1e68
-bafae38c2e3ac848a48f89a12a065a501d2a0511
+82a6d07a37d13c247e84417055f9cb5b4802ac4f
+47780b2c59a8356c1f13cd7c8d250e3250c15ba8
+28bd1a044251fca35e14ba55d7ddb567deadcdaa
 a7c32f24cd5b69b237dc974804a71326306f4e58
 ---
 ## Optional hash table with comma-separated file list
@@ -34,8 +33,7 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 20444b8e9382e659360a1446688d10a8c2d2ad31: github-copilot: add v1.0.8
 ## f5742718da7bd1d078ddc8423011a82ef2e3c759: gaudi: workaround test-dependency bug with a when
 ## deb4f17d93dbe012403614245334f7c73fcc086f: fix: add latest osg-ca-cert
-## 0d64b2bab72a99441f042b663e4bc993ec0db45d: osg-ca-certs: depends on gmake and perl, type build
-## a77a7ed0d2630466cac71165026387b1381b058a: py-pynacl: depends on gmake, type build
-## bb6cd78da2a0c213508820fd27e322f5582e1e68: py-throttler: add v1.2.3
-## bafae38c2e3ac848a48f89a12a065a501d2a0511: py-throttler: fix typo
+## 82a6d07a37d13c247e84417055f9cb5b4802ac4f: osg-ca-certs: depends on gmake and perl, type build
+## 47780b2c59a8356c1f13cd7c8d250e3250c15ba8: py-pynacl: depends on gmake, type build
+## 28bd1a044251fca35e14ba55d7ddb567deadcdaa: py-throttler: add v1.2.3
 ## a7c32f24cd5b69b237dc974804a71326306f4e58: py-tensorboard: add v2.21.0
