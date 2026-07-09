0d6260977f5449e41427d9020268d0934ea706e1
Dmitry Kalinkin <dkalinkin1@bnl.gov>
2026-07-09 03:19:04 -0400

    feat: eic-smear 1.2.2 (#346)
    * feat: eic-smear 1.2.2
    
    * bump eic-spack

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="3300e7e4dcf3c28b08ac9fc01c553edf935a5b63"
+EICSPACK_VERSION="f0035c7f4bb8c96cfbaa8f7bdcbb28f83ffb70b5"
