f2e75c2c8e57ab0e30fe247a8ef5d0c8638932a0
SebastianPaucar <paucar.sebastian@hotmail.com>
2026-08-09 12:11:43 -0600

    test: point spack.sh at debuggable-installations fork/branch for local validation

--- spack.sh
+++ spack.sh
@@ -1,17 +1,15 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034  # Variables are used by scripts that source this file
 ## Spack organization and repository, e.g. spack/spack
-SPACK_ORGREPO="spack/spack"
+SPACK_ORGREPO="SebastianPaucar/spack"
 
 ## Spack github version, e.g. v0.18.1 or commit hash
 ## note: nightly builds will use e.g. releases/v1.0
-SPACK_VERSION="v1.2.2"
+SPACK_VERSION="feature/debuggable-installations-v1.2.2"
 
 ## Space-separated list of spack cherry-picks
 read -r -d '' SPACK_CHERRYPICKS <<- \
 --- || true
-292b0dcaba3b2a5e3f9668d205d39fee2c715721
-678e506a95b319c573ba7e84703b06d7275ab80e
 ---
 ## Optional hash table with comma-separated file list
 read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
