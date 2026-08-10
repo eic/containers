f69b9755208676670980aeb22946a11458939c0d
SebastianPaucar <paucar.sebastian@hotmail.com>
2026-08-09 19:12:10 -0600

    added: spack.sh description

--- spack.sh
+++ spack.sh
@@ -15,6 +15,10 @@ read -r -d '' SPACK_CHERRYPICKS <<- \
 read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 --- || true
 ---
+## Ref: this branch is v1.2.2 + the two upstream fixes below, cherry-picked
+## directly onto the branch (since SPACK_ORGREPO no longer points at
+## spack/spack, the normal cherry-pick mechanism can't fetch them) + the
+## debuggable-installations feature work.
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
 ## 292b0dcaba3b2a5e3f9668d205d39fee2c715721: fix: write created time field with OCI buildcache config
