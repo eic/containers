e07a13078d52a2bba16e1ab03022f6003f1e9c53
SebastianPaucar <paucar.sebastian@hotmail.com>
2026-08-25 15:13:13 -0600

    spack-packages.sh: cherry-pick compiler-wrapper@1.1.0-build-id prototype. spack.sh: cherry-pick full debuggable framework

--- spack.sh
+++ spack.sh
@@ -1,25 +1,26 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034  # Variables are used by scripts that source this file
 ## Spack organization and repository, e.g. spack/spack
-SPACK_ORGREPO="SebastianPaucar/spack"
+SPACK_ORGREPO="spack/spack"
 
 ## Spack github version, e.g. v0.18.1 or commit hash
 ## note: nightly builds will use e.g. releases/v1.0
-SPACK_VERSION="feature/debuggable-installations-v1.2.2"
+SPACK_VERSION="v1.2.2"
 
 ## Space-separated list of spack cherry-picks
 read -r -d '' SPACK_CHERRYPICKS <<- \
 --- || true
+292b0dcaba3b2a5e3f9668d205d39fee2c715721
+678e506a95b319c573ba7e84703b06d7275ab80e
+2484c2b9387853ee3f81bb7b10d9bfd4c79cf190
 ---
 ## Optional hash table with comma-separated file list
 read -r -d '' SPACK_CHERRYPICKS_FILES <<- \
 --- || true
 ---
-## Ref: this branch is v1.2.2 + the two upstream fixes below, cherry-picked
-## directly onto the branch (since SPACK_ORGREPO no longer points at
-## spack/spack, the normal cherry-pick mechanism can't fetch them) + the
-## debuggable-installations feature work.
 ## Ref: https://github.com/spack/spack/commit/[hash]
 ## [hash]: [description]
 ## 292b0dcaba3b2a5e3f9668d205d39fee2c715721: fix: write created time field with OCI buildcache config
 ## 678e506a95b319c573ba7e84703b06d7275ab80e: fix: don't map prefix to view root for pkgs excluded from view
+## 2484c2b9387853ee3f81bb7b10d9bfd4c79cf190: feat: debuggable installations (source hook, symbol
+##   splitting, gdbinit, OCI autopush), squashed and cherry-picked via open draft PR spack/spack#52949
