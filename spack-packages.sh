c24d1a8103b243d0753d79cac486d07d3292f65f
Wouter Deconinck <wdconinc@gmail.com>
2026-08-28 12:44:51 -0500

    fix: spack cherry-pick: podio: add a patch to be able to build with fmt 12

--- spack-packages.sh
+++ spack-packages.sh
@@ -38,9 +38,10 @@ b7819dfb21e2716761e0a99bc93a851eef0e6343
 0e793b360b1383044923a19146f7d977f3107a41
 4c53d69e7e02fb346500b6269e43a26d1010d0f7
 ded706e8eb08bae4ac1cd4618c594bee4040a00e
-e2a49ec27bbe7a64f1388d296d139754af5f79f4
-5d79a44c68046e528f78b3f5e4b42754c1e05c93
-818143ef3d6c7f52474e8ff22472247b94ace049
+c44cd71db91d6e6c68cb604dcb87311b49b1bedb
+33d01efa54ffc5b211cd5abc2df74841c032a722
+7e581611d81312d2a4228ad5d0b67bdd93217915
+683c5a698d88d657a99f3adefee0fe98d30927ae
 409826414a2221be56011f8f0f912905dae558cd
 ---
 ## Optional hash table with comma-separated file list
@@ -77,7 +78,8 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 0e793b360b1383044923a19146f7d977f3107a41: pandora*: Update PandoraPFA repository URLs
 ## 4c53d69e7e02fb346500b6269e43a26d1010d0f7: pandora*: add v5.*
 ## ded706e8eb08bae4ac1cd4618c594bee4040a00e: acts: pass root variant to examples
-## e2a49ec27bbe7a64f1388d296d139754af5f79f4: podio: add arrow variant
-## 5d79a44c68046e528f78b3f5e4b42754c1e05c93: podio,sio: url_for_version for main version
-## 818143ef3d6c7f52474e8ff22472247b94ace049: dd4hep: url_for_version for master version
+## c44cd71db91d6e6c68cb604dcb87311b49b1bedb: podio: add a patch to be able to build with fmt 12
+## 33d01efa54ffc5b211cd5abc2df74841c032a722: podio: add arrow variant
+## 7e581611d81312d2a4228ad5d0b67bdd93217915: podio,sio: url_for_version for main version
+## 683c5a698d88d657a99f3adefee0fe98d30927ae: dd4hep: url_for_version for master version
 ## 409826414a2221be56011f8f0f912905dae558cd: podio: Add version 1.8 and new parquet variant
