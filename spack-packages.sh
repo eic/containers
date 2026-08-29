3d05d87c22ad786b3d4616540f586c4e38ad5b80
Wouter Deconinck <wdconinc@gmail.com>
2026-08-29 18:58:13 -0500

    chore: root-6.40.04 (#401)
    * fix: spack cherry-pick: root: add v6.40.04
    
    * chore: root-6.40.04

--- spack-packages.sh
+++ spack-packages.sh
@@ -38,6 +38,7 @@ b7819dfb21e2716761e0a99bc93a851eef0e6343
 0e793b360b1383044923a19146f7d977f3107a41
 4c53d69e7e02fb346500b6269e43a26d1010d0f7
 ded706e8eb08bae4ac1cd4618c594bee4040a00e
+8090c2e88e44621599b1e6e2a1f6243fd8ec78d3
 c44cd71db91d6e6c68cb604dcb87311b49b1bedb
 33d01efa54ffc5b211cd5abc2df74841c032a722
 7e581611d81312d2a4228ad5d0b67bdd93217915
@@ -79,6 +80,7 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 0e793b360b1383044923a19146f7d977f3107a41: pandora*: Update PandoraPFA repository URLs
 ## 4c53d69e7e02fb346500b6269e43a26d1010d0f7: pandora*: add v5.*
 ## ded706e8eb08bae4ac1cd4618c594bee4040a00e: acts: pass root variant to examples
+## 8090c2e88e44621599b1e6e2a1f6243fd8ec78d3: root: add v6.40.04
 ## c44cd71db91d6e6c68cb604dcb87311b49b1bedb: podio: add a patch to be able to build with fmt 12
 ## 33d01efa54ffc5b211cd5abc2df74841c032a722: podio: add arrow variant
 ## 7e581611d81312d2a4228ad5d0b67bdd93217915: podio,sio: url_for_version for main version
