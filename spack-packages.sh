61482c28868d62411f323e6ac96efd94be58f03b
SebastianPaucar <paucar.sebastian@hotmail.com>
2026-08-29 18:54:25 -0600

    spack-packages.sh: remove stale compiler-wrapper cherry-pick hash (2700b8c7 predates amend, 4b54f295 is current PR #6214 head)

--- spack-packages.sh
+++ spack-packages.sh
@@ -48,7 +48,6 @@ c44cd71db91d6e6c68cb604dcb87311b49b1bedb
 9385426b49d2c49d026629cb5e96bb5893824e3d
 d1bfcea155bb51220a4baddfcc75a0ba4d4b972c
 f3085a0fd9c327692475fbb72b8b9f738c541358
-2700b8c7c7902cc9b3d19af1a57183bdeaf21c44
 4b54f295bed925f1a33f3954bd05903e53142406
 5945d81a8359eed559ec60b1be9151de57473f51
 ---
@@ -96,6 +95,6 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## 9385426b49d2c49d026629cb5e96bb5893824e3d: opencascade: add v7.9.2, v7.9.3
 ## d1bfcea155bb51220a4baddfcc75a0ba4d4b972c: opencascade: add v8.0.0, v8.0.0.p1
 ## f3085a0fd9c327692475fbb72b8b9f738c541358: opencascade: pass tcl library paths to the cmake build to fix mac builds
-## 2700b8c7c7902cc9b3d19af1a57183bdeaf21c44: compiler-wrapper: add 1.1.0-build-id prototype version (spack-packages#6214)
 ## 4b54f295bed925f1a33f3954bd05903e53142406: compiler-wrapper: add 1.1.0-build-id prototype version (spack-packages#6214)
 ## 5945d81a8359eed559ec60b1be9151de57473f51: elfutils: patch debuginfod_find_source to accept ./-relative filenames (spack-packages #6259)
+ 
