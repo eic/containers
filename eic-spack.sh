2cf408b1bc713ed665892220dd7a93eea7b90078
Wouter Deconinck <wdconinc@gmail.com>
2026-09-03 14:37:32 -0500

    fix: eic-spack: eiccontent: add actual release version for SBOM

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="aed46e4c8ef09d12d60418febbac17da45de7001"
+EICSPACK_VERSION="fac06d328972636acdb8391c85a1bb1db54342a5"
