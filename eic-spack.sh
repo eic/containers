88a640f1bb5edfc2f6bc150240ad462766d7f28c
Wouter Deconinck <wdconinc@gmail.com>
2026-09-04 15:05:34 -0500

    fix: eic-spack: iccontent: new PandoraPFA support package for EIC

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="fac06d328972636acdb8391c85a1bb1db54342a5"
+EICSPACK_VERSION="f6f234940d38660932ae04e7a7bdb2c320c03762"
