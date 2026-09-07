b73ff694f79c41df2d7933a2b15acb1831565df2
Wouter Deconinck <wdconinc@gmail.com>
2026-09-07 17:45:56 -0500

    fix: eic-spack: EIC MCP servers and the eic-mcp launcher

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,5 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-## eic-spack#986 head
-EICSPACK_VERSION="73a53adb033fe05e592a65038b14248c25cd1ad9"
+EICSPACK_VERSION="0d7cc5f5b294d7189fd28e31150f80afbb31ae60"
