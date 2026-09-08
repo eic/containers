a64422ec93633261016929a7646cde81e349540c
SebastianPaucar <paucar.sebastian@hotmail.com>
2026-09-07 22:44:25 -0600

    moving debuggable spack installations framework into xl env for root, geant4 and their xl dependents

--- scripts/build-eic.sh
+++ scripts/build-eic.sh
@@ -207,7 +207,7 @@ for build_type in "${BUILD_TYPES[@]}"; do
   ## Compute install flags: base flags plus debug capture for the dbg environment
   SPACK_INSTALL_FLAGS="--no-check-signature --show-log-on-error --yes-to-all"
   SPACK_BUILDER_INSTALL_FLAGS="${SPACK_INSTALL_FLAGS}"
-  if [ "${ENV}" = "dbg" ]; then
+  if [ "${ENV}" = "dbg" ] || [ "${ENV}" = "xl" ]; then
     SPACK_BUILDER_INSTALL_FLAGS="${SPACK_BUILDER_INSTALL_FLAGS} --no-cache --debug-source --debug-symbols"
   fi
   build_cmd+=(--build-arg "SPACK_INSTALL_FLAGS=${SPACK_INSTALL_FLAGS}")
