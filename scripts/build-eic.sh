3b5d1db693a16af6bb7f74aa9169329572e9951c
SebastianPaucar <paucar.sebastian@hotmail.com>
2026-09-09 11:54:03 -0600

    fix: drop --no-cache (no needed for xl)

--- scripts/build-eic.sh
+++ scripts/build-eic.sh
@@ -208,7 +208,7 @@ for build_type in "${BUILD_TYPES[@]}"; do
   SPACK_INSTALL_FLAGS="--no-check-signature --show-log-on-error --yes-to-all"
   SPACK_BUILDER_INSTALL_FLAGS="${SPACK_INSTALL_FLAGS}"
   if [ "${ENV}" = "dbg" ] || [ "${ENV}" = "xl" ]; then
-    SPACK_BUILDER_INSTALL_FLAGS="${SPACK_BUILDER_INSTALL_FLAGS} --no-cache --debug-source --debug-symbols"
+    SPACK_BUILDER_INSTALL_FLAGS="${SPACK_BUILDER_INSTALL_FLAGS} --debug-source --debug-symbols"
   fi
   build_cmd+=(--build-arg "SPACK_INSTALL_FLAGS=${SPACK_INSTALL_FLAGS}")
   build_cmd+=(--build-arg "SPACK_BUILDER_INSTALL_FLAGS=${SPACK_BUILDER_INSTALL_FLAGS}")
