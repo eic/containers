679c0cf80adf93a76cf602d22b10d369fa7742dd
Wouter Deconinck <wdconinc@gmail.com>
2026-09-23 13:38:52 -0500

    feat: eic-spack: irt2: add 2.2.2

--- eic-spack.sh
+++ eic-spack.sh
@@ -5,4 +5,4 @@ EICSPACK_ORGREPO="eic/eic-spack"
 
 ## EIC spack commit hash or github version, e.g. v0.19.7
 ## note: nightly builds could use a branch e.g. releases/v0.19
-EICSPACK_VERSION="f9c4922e8c3cec1b7f8a541445e8c49137a0adac"
+EICSPACK_VERSION="8a7edff9b5887d4e70a7022fccda2afb8552ba57"
